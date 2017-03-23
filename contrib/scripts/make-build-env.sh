05df86d0180cc578d28598eeb0094248cce82a69
Martin Eden <martin.s.eden@gmail.com>
2017-03-23 15:08:40 +0000

    Fix backslash (grr Windows)

--- contrib/scripts/make-build-env.sh
+++ contrib/scripts/make-build-env.sh
@@ -1,3 +1,3 @@
 set -e
 git_id=$(git rev-parse --short HEAD)
-docker build -f .\build.dockerfile -t montagu-contrib-portal-build-env --build-arg app_docker_version=$git_id .
+docker build -f ./build.dockerfile -t montagu-contrib-portal-build-env --build-arg app_docker_version=$git_id .
