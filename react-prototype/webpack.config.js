f095361dbceeddb95cdaf7ce385203969d47d3f2
Martin Eden <martin.s.eden@gmail.com>
2017-03-24 12:58:38 +0000

    Prototype: How to integrate Babel and Typescript

--- react-prototype/webpack.config.js
+++ react-prototype/webpack.config.js
@@ -19,16 +19,18 @@ module.exports = {
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            // and then fed through Babel to compiled from ES6 to ES5y
             { 
                 test: /\.tsx?$/, 
-                loader: "awesome-typescript-loader" 
+                loaders: [ "babel-loader", "awesome-typescript-loader" ],                
+                exclude: [ /node_modules/ ]
             },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { 
                 enforce: 'pre',
                 test: /\.js$/, 
                 loader: "source-map-loader" 
-            }
+            },
         ]
     },
 
