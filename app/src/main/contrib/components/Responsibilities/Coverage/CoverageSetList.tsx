576ebd860d415cf07996e49dd07883632e708915
Martin Eden <martin.s.eden@gmail.com>
2017-06-23 16:29:09 +0100

    Correctly restore page state after logout and login

--- app/src/main/contrib/components/Responsibilities/Coverage/CoverageSetList.tsx
+++ app/src/main/contrib/components/Responsibilities/Coverage/CoverageSetList.tsx
@@ -11,23 +11,27 @@ interface Props {
 
 export class CoverageSetList extends React.Component<Props, undefined> {
     render() {
-        const coverageSets = this.props.coverageSets.map((x, order) =>
-            <CoverageSetComponent key={ order } order={ order } set={ x } />
-        );
-        return <div className={[ styles.coverageSets, commonStyles.gapAbove ].join(" ")}>
-            <div>These coverage sets must be applied in the following order:</div>
-            <table>
-                <thead>
-                <tr>
-                    <th>Order</th>
-                    <th>Description</th>
-                    <th>Vaccine</th>
-                    <th>Activity type</th>
-                    <th>GAVI support level</th>
-                </tr>
-                </thead>
-                <tbody>{ coverageSets }</tbody>
-            </table>
-        </div>;
+        if (this.props.coverageSets) {
+            const coverageSets = this.props.coverageSets.map((x, order) =>
+                <CoverageSetComponent key={ order } order={ order } set={ x }/>
+            );
+            return <div className={[styles.coverageSets, commonStyles.gapAbove].join(" ")}>
+                <div>These coverage sets must be applied in the following order:</div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Order</th>
+                        <th>Description</th>
+                        <th>Vaccine</th>
+                        <th>Activity type</th>
+                        <th>GAVI support level</th>
+                    </tr>
+                    </thead>
+                    <tbody>{ coverageSets }</tbody>
+                </table>
+            </div>;
+        } else {
+            return null;
+        }
     }
 }
