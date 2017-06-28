bc63cfc6bfeab183a30666a4e0a2e7f62bcde4d5
Alex Hill <alex.hill@gmail.com>
2017-06-28 15:32:56 +0100

    add test for view all users page

---
+++ app/src/test/admin/components/Users/List/ViewAllUsersPageTests.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { expect } from "chai";
+import { Sandbox } from "../../../../Sandbox";
+import { mockLocation } from "../../../../mocks/mocks";
+import { checkAsync } from "../../../../testHelpers";
+import {ViewAllUsersPage} from "../../../../../main/admin/components/Users/List/ViewAllUsersPage";
+import {userStore} from "../../../../../main/admin/stores/UserStore";
+
+describe("ViewAllModellingGroupsPageTests", () => {
+    const sandbox = new Sandbox();
+
+    afterEach(() => sandbox.restore());
+
+    it("triggers fetch on load", (done: DoneCallback) => {
+        const spy = sandbox.sinon.spy(userStore, "fetchUsers");
+        sandbox.mount(<ViewAllUsersPage location={ mockLocation<undefined>() } />);
+        checkAsync(done, () => {
+            expect(spy.called).to.equal(true, "Expected usersStore.fetchUsers to be triggered");
+        });
+    });
+});
