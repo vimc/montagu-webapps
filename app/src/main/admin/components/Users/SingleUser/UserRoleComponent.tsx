a43605d9cb58db028d2ae0bbfc6f47704c2c149d
Alex Hill <alex.hill@gmail.com>
2017-06-30 13:23:04 +0100

    braces round if statement

--- app/src/main/admin/components/Users/SingleUser/UserRoleComponent.tsx
+++ app/src/main/admin/components/Users/SingleUser/UserRoleComponent.tsx
@@ -5,9 +5,9 @@ export class UserRole extends React.Component<RoleAssignment, undefined> {
 
     render() {
         let scope = "";
-        if (this.props.scope_prefix && this.props.scope_prefix.length > 0)
+        if (this.props.scope_prefix && this.props.scope_prefix.length > 0) {
             scope = " / " + this.props.scope_prefix + ":" + this.props.scope_id;
-
+        }
         return <div>
                 { this.props.name }{ scope }
             </div>
