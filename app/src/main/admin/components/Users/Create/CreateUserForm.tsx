e2dd371d0ba2d8f182ed091b22a5b7350ead3eda
Martin Eden <martin.s.eden@gmail.com>
2017-08-24 14:20:56 +0100

    Disable user create form while request is pending

--- app/src/main/admin/components/Users/Create/CreateUserForm.tsx
+++ app/src/main/admin/components/Users/Create/CreateUserForm.tsx
@@ -22,32 +22,34 @@ export class CreateUserFormComponent extends React.Component<ReformProps, undefi
     render() {
         const fields = this.props.fields as CreateUserFields;
         return <form className={commonStyles.gapAbove} onSubmit={this.props.submit}>
-            <div className={commonStyles.sectionTitle}>Add new user</div>
-            <table className={formStyles.tableForm}>
-                <tbody>
-                <tr>
-                    <td>Full name</td>
-                    <td><input name="name" type="string" {...fields.name} onChange={this.changeName} /></td>
-                    <td><ValidationError message={this.props.errors.name}/></td>
-                </tr>
-                <tr>
-                    <td>Email address</td>
-                    <td><input name="email" type="email" {...fields.email} /></td>
-                    <td><ValidationError message={this.props.errors.email}/></td>
-                </tr>
-                <tr>
-                    <td>Username</td>
-                    <td><input name="username" type="string" {...fields.username} /></td>
-                    <td><ValidationError message={this.props.errors.username}/></td>
-                </tr>
-                </tbody>
-            </table>
-            <div className={commonStyles.gapAbove}>
-                <ValidationError message={this.props.store.state.submitError}/>
-            </div>
-            <div className={commonStyles.gapAbove}>
-                <button type="submit">Save user</button>
-            </div>
+            <fieldset disabled={this.props.loading}>
+                <div className={commonStyles.sectionTitle}>Add new user</div>
+                <table className={formStyles.tableForm}>
+                    <tbody>
+                    <tr>
+                        <td>Full name</td>
+                        <td><input name="name" {...fields.name} onChange={this.changeName} /></td>
+                        <td><ValidationError message={this.props.errors.name}/></td>
+                    </tr>
+                    <tr>
+                        <td>Email address</td>
+                        <td><input name="email" type="email" {...fields.email} /></td>
+                        <td><ValidationError message={this.props.errors.email}/></td>
+                    </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td><input name="username" {...fields.username} /></td>
+                        <td><ValidationError message={this.props.errors.username}/></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <div className={commonStyles.gapAbove}>
+                    <ValidationError message={this.props.store.state.submitError}/>
+                </div>
+                <div className={commonStyles.gapAbove}>
+                    <button type="submit">Save user</button>
+                </div>
+            </fieldset>
         </form>;
     }
 }
