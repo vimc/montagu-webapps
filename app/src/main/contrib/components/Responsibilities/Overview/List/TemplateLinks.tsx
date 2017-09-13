be3bd12e5411477441ea42c479331537ddfde8b2
Alex Hill <alex.hill@gmail.com>
2017-09-13 10:46:19 +0100

    group prefixed tempalte names

--- app/src/main/contrib/components/Responsibilities/Overview/List/TemplateLinks.tsx
+++ app/src/main/contrib/components/Responsibilities/Overview/List/TemplateLinks.tsx
@@ -1,23 +1,30 @@
 import * as React from "react";
 import { mainStore } from "../../../../stores/MainStore";
-import { IExtendedResponsibilitySet } from "../../../../models/ResponsibilitySet";
+import { ExtendedResponsibility, IExtendedResponsibilitySet } from "../../../../models/ResponsibilitySet";
 import { ButtonLink } from "../../../../../shared/components/ButtonLink";
 
-export class TemplateLinks extends React.Component<IExtendedResponsibilitySet, undefined> {
+interface TemplateLinksProps {
+    responsibilities: ExtendedResponsibility[];
+    groupId: String
+}
+
+export class TemplateLinks extends React.Component<TemplateLinksProps, undefined> {
     render(): JSX.Element {
+
         const diseaseIds = [ ...new Set(this.props.responsibilities.map(x => x.scenario.disease)) ];
 
         if (diseaseIds.length > 0) {
             const links = diseaseIds
                 .map(id => mainStore.getDiseaseById(id))
-                .map(disease => <ButtonLink key={disease.id} href={`/templates/${disease.id}.csv`}>{disease.name}</ButtonLink> );
+                .map(disease => <ButtonLink key={disease.id}
+                                            href={`/templates/${this.props.groupId}-${disease.id}.csv`}>{disease.name}</ButtonLink>);
 
             return <div>
-                Download burden estimate templates:<br />
+                Download burden estimate templates:<br/>
                 {links}
             </div>;
         } else {
-            return <span />;
+            return <span/>;
         }
     }
 
