8483bf68090e04316ad5606f0bddf238a60fd046
Alex Hill <alex.hill@gmail.com>
2017-09-20 11:08:32 +0100

    remove unused action

--- app/src/main/contrib/actions/EstimateActions.ts
+++ app/src/main/contrib/actions/EstimateActions.ts
@@ -1,14 +1,7 @@
 import { FetchActions, FetchActionsInterface } from "../../shared/actions/FetchActions";
 import { alt } from "../../shared/alt";
 
-interface Actions extends FetchActionsInterface<string> {
-    clearUsedToken(): boolean;
+class EstimateTokenActions extends FetchActions<string> implements FetchActionsInterface<string>  {
 }
 
-class EstimateTokenActions extends FetchActions<string> implements Actions {
-    clearUsedToken(): boolean {
-        return true;
-    }
-}
-
-export const estimateTokenActions = alt.createActions<Actions>(EstimateTokenActions);
+export const estimateTokenActions = alt.createActions<FetchActionsInterface<string>>(EstimateTokenActions);
