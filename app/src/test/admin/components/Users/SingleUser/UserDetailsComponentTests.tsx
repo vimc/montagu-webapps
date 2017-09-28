9463b1b5c138ea97fe614a98cc34843ba157553a
Alex Hill <alex.hill@gmail.com>
2017-09-28 18:57:45 +0100

    tests

--- app/src/test/admin/components/Users/SingleUser/UserDetailsComponentTests.tsx
+++ app/src/test/admin/components/Users/SingleUser/UserDetailsComponentTests.tsx
@@ -5,6 +5,7 @@ import {mockUser} from "../../../../mocks/mockModels";
 import {alt} from "../../../../../main/shared/alt";
 
 import { UserDetailsContentComponent } from "../../../../../main/admin/components/Users/SingleUser/UserDetailsContent";
+import { AddRoles } from "../../../../../main/admin/components/Users/SingleUser/AddRoles";
 
 describe("UserDetailsComponent", () => {
     it("can get props from stores", () => {
@@ -31,5 +32,31 @@ describe("UserDetailsComponent", () => {
     it("can render", () => {
         const user = mockUser({username: "tets.user"});
         shallow(<UserDetailsContentComponent ready={true} user={user} roles={[]} />);
+    });
+
+    it("show add role widget if logged in user has roles.write permission", () => {
+        const user = mockUser({username: "tets.user"});
+        alt.bootstrap(JSON.stringify({
+            AdminAuthStore: {
+                permissions: ["*/can-login", "*/roles.write"]
+            }
+        }));
+
+        const result = shallow(<UserDetailsContentComponent ready={true} user={user} roles={[]} />);
+
+        expect(result.find(AddRoles).length).to.eq(1)
+    });
+
+    it("does not show add role widget if logged in user does not have roles.write permission", () => {
+        const user = mockUser({username: "tets.user"});
+        alt.bootstrap(JSON.stringify({
+            AdminAuthStore: {
+                permissions: ["*/can-login"]
+            }
+        }));
+
+        const result = shallow(<UserDetailsContentComponent ready={true} user={user} roles={[]} />);
+
+        expect(result.find(AddRoles).length).to.eq(0)
     });
 });
