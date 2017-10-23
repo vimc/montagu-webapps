bee207f3abf9d35432b0caa1c332b214242a777d
Martin Eden <martin.s.eden@gmail.com>
2017-10-23 12:20:54 +0100

    Only add test users if they don't already exist

--- app/scripts/add-test-accounts-for-integration-tests.sh
+++ app/scripts/add-test-accounts-for-integration-tests.sh
@@ -2,13 +2,17 @@
 set -ex
 here=$(dirname $0)
 
-$here/cli.sh add "Test User" test.user test@example.com password
+$here/cli.sh add "Test User" test.user \
+    test@example.com password \
+    --if-not-exists
 $here/cli.sh addRole test.user user
 $here/cli.sh addRole test.user user-manager
 $here/cli.sh addRole test.user uploader modelling-group:IC-Garske
 $here/cli.sh addRole test.user user-manager modelling-group:IC-Garske
 $here/cli.sh addUserToGroup test.user test-group
 
-$here/cli.sh add "Report reviewer" report.reviewer report.reviewer@example.com password
+$here/cli.sh add "Report reviewer" report.reviewer \
+    report.reviewer@example.com password \
+    --if-not-exists
 $here/cli.sh addRole report.reviewer user
 $here/cli.sh addRole report.reviewer reports-reviewer
