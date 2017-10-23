a53bca3ae396be974f191e65891a4d92c5687f36
Martin Eden <martin.s.eden@gmail.com>
2017-10-23 16:23:44 +0100

    Get the id and branch in the integration tests script too

--- app/scripts/make-integration-tests-image.sh
+++ app/scripts/make-integration-tests-image.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+git_id=$(git rev-parse --short HEAD)
+git_branch=$(git symbolic-ref --short HEAD)
 registry=docker.montagu.dide.ic.ac.uk:5000
 name=$registry/montagu-portal-integration-tests
 
