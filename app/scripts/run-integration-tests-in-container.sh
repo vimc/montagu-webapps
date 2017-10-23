73602afc1aa9f81ff65b1eb019ae522d9ab3186e
Martin Eden <martin.s.eden@gmail.com>
2017-10-23 16:14:50 +0100

    Specify environment variables closer to where they are used.

--- app/scripts/run-integration-tests-in-container.sh
+++ app/scripts/run-integration-tests-in-container.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -ex
+
+export PGHOST=db
+export MONTAGU_PORTAL_PROFILE=packaged_integration_tests
 ./scripts/add-test-accounts-for-integration-tests.sh
 ./scripts/run-integration-tests.sh "$@"
