65c240ccbbeff118503315be1500e963e01abf7f
Martin Eden <martin.s.eden@gmail.com>
2017-10-23 16:13:38 +0100

    Rename local images

--- app/scripts/make-build-env.sh
+++ app/scripts/make-build-env.sh
@@ -6,12 +6,12 @@ git_branch=$(git symbolic-ref --short HEAD)
 # Shared build env between the main build (being executed now) and the 
 # reusable integration test image that will be run in the main Montagu build.
 docker build -f ./docker/shared-build-env.dockerfile \
-    -t montagu-portal-build-env \
+    -t montagu-portal-shared-build-env \
     .
 
 # The main build env which builds and tests in the next step
 docker build -f ./docker/build.dockerfile \
-    -t montagu-portal-build \
+    -t montagu-portal-build-env \
     --build-arg MONTAGU_GIT_ID=$git_id \
     --build-arg MONTAGU_GIT_BRANCH=$git_branch \
     .
