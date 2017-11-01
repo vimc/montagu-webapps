4e348889961d36cb4e4f085e726a7b73b20beb07
Martin Eden <martin.s.eden@gmail.com>
2017-11-01 16:55:40 +0000

    WIP

--- app/src/test/shared/components/NavigationTests.ts
+++ app/src/test/shared/components/NavigationTests.ts
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import {Breadcrumb} from "../../../main/shared/models/Breadcrumb";
 import {PageWithHeader} from "../../../main/shared/components/PageWithHeader/PageWithHeader";
 import {checkAsync} from "../../testHelpers";
-import {expectNoActions, getActions} from "../../actionHelpers";
+import {Action, expectNoActions, getActions} from "../../actionHelpers";
 import {Sandbox} from "../../Sandbox";
 import {navActions} from "../../../main/shared/actions/NavActions";
 
@@ -15,7 +15,12 @@ export interface PageNavigationExpectation {
 export abstract class NavigationTests {
     abstract pages(): PageNavigationExpectation[];
 
-    checkCanNavigateTo(expectation: PageNavigationExpectation, done: DoneCallback, sandbox: Sandbox) {
+    checkCanNavigateTo(
+        expectation: PageNavigationExpectation,
+        done: DoneCallback,
+        sandbox: Sandbox,
+        getCrumbFromAction: (action: Action) => Breadcrumb
+    ) {
         const spy = sandbox.dispatchSpy();
         const page = expectation.page;
         page.load();
@@ -27,25 +32,38 @@ export abstract class NavigationTests {
                 expect(actions).to.have.length.greaterThan(0, "There were no actions");
                 const lastAction = actions[actions.length - 1];
 
-                const crumb = lastAction.payload as Breadcrumb;
+                const crumb = getCrumbFromAction(lastAction.payload);
                 if (expectation.urlShouldBeNull === true) {
                     expect(crumb.url).to.equal(null, "Expected URL to be null");
                 } else {
                     expect(crumb.url).to.not.equal(null, "URL was null");
+                    expect(crumb.url).to.not.equal(undefined, "URL was undefined");
                 }
                 expect(crumb.name).to.not.be.equal(null, "Name was null");
+                expect(crumb.name).to.not.equal(undefined, "Name was undefined");
             }
         });
     };
 
     addTestsToMocha() {
         const sandbox = new Sandbox();
-        beforeEach(() => navActions.initialize(null));
         afterEach(() => sandbox.restore());
 
         this.pages().forEach(expectation => {
-            it("can navigate to " + expectation.page.constructor.name, (done: DoneCallback) => {
-                this.checkCanNavigateTo(expectation, done, sandbox);
+            const name = expectation.page.constructor.name;
+            it("can navigate to " + name, (done: DoneCallback) => {
+                navActions.initialize(null);
+                this.checkCanNavigateTo(expectation, done, sandbox, a => {
+                    expect(a.action).to.equal("NavActions.navigate");
+                    return a.payload as Breadcrumb;
+                });
+            });
+            it("can initialize at " + name, (done: DoneCallback) => {
+                this.checkCanNavigateTo(expectation, done, sandbox, a => {
+                    expect(a.action).to.equal("NavActions.initialize");
+                    const crumbs = a.payload as Breadcrumb[];
+                    return crumbs[crumbs.length - 1];
+                });
             });
         })
     }
