91d0c2f0cb715ff787c9f9e494899dce9e1dba70
Martin Eden <martin.s.eden@gmail.com>
2017-11-03 14:02:12 +0000

    New tests.

--- app/src/test/report/components/ReportListItemTests.tsx
+++ app/src/test/report/components/ReportListItemTests.tsx
@@ -5,9 +5,13 @@ import {ReportListItem} from "../../../main/report/components/Reports/ReportList
 import {Report} from "../../../main/shared/models/Generated";
 import {InternalLink} from "../../../main/shared/components/InternalLink";
 
+interface LinkProps {
+    url: string;
+    text: string;
+}
 
 describe("ReportListItem", () => {
-    const render = function(props: Partial<Report>): string {
+    const render = function(props: Partial<Report>): LinkProps {
         const defaultProps: Report = {
             name: "name",
             display_name: "display name",
@@ -15,22 +19,31 @@ describe("ReportListItem", () => {
         };
         const combined: Report = Object.assign({}, defaultProps, props);
         const rendered = shallow(<ReportListItem {...combined} />);
-        return rendered.find(InternalLink).children().text();
+        const link = rendered.find(InternalLink);
+        return {
+            url: link.prop("href"),
+            text: link.children().text()
+        };
     };
 
 
     it("renders display name if possible", () => {
-        expect(render({ display_name: "display name" }))
+        expect(render({ display_name: "display name" }).text)
             .to.equal("display name");
     });
 
     it("renders short name if display name is empty", () => {
-        expect(render({ name: "name", display_name: "" }))
+        expect(render({ name: "name", display_name: "" }).text)
             .to.equal("name");
     });
 
     it("renders short name if display name is null", () => {
-        expect(render({ name: "name", display_name: null }))
+        expect(render({ name: "name", display_name: null }).text)
             .to.equal("name");
+    });
+
+    it("links to latest report version", () => {
+        expect(render({ name: "name", latest_version: "some_version" }).url)
+            .to.equal("/name/some_version/");
     });
 });
