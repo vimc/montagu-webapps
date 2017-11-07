bfc7650673db78b2ef62f5072bf29eee6e06fd34
Alex Hill <alex.hill@gmail.com>
2017-11-07 11:19:29 +0000

    redirect url working

--- app/src/main/contrib/sources/EstimatesTokenSource.ts
+++ app/src/main/contrib/sources/EstimatesTokenSource.ts
@@ -2,6 +2,7 @@ import { CoverageSource } from "./CoverageSource";
 import SourceModel = AltJS.SourceModel;
 import { estimateTokenActions } from "../actions/EstimateActions";
 import {appSettings} from "../../shared/Settings";
+import { settings } from "../../shared/Settings";
 
 export class EstimatesTokenSource extends CoverageSource {
     fetchOneTimeEstimatesToken: () => SourceModel<string>;
@@ -9,7 +10,7 @@ export class EstimatesTokenSource extends CoverageSource {
     constructor() {
         super();
         this.fetchOneTimeEstimatesToken = () => this.doFetch(state => {
-            const redirectUrl = encodeURI(state.redirectUrl);
+            const redirectUrl = encodeURI(settings.montaguUrl() + appSettings.publicPath + state.redirectUrl);
             return this.baseURL(state) + `/estimates/get_onetime_link/?redirectUrl=${redirectUrl}/`;
         }, {
             success: estimateTokenActions.update,
