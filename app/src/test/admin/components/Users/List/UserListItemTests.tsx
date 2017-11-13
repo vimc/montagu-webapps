71b016ca8afbe01ab51aa7c08215af8e0db26973
Alex Hill <alex.hill@gmail.com>
2017-11-13 18:08:33 +0000

    convert user name dots to hypens for route

--- app/src/test/admin/components/Users/List/UserListItemTests.tsx
+++ app/src/test/admin/components/Users/List/UserListItemTests.tsx
@@ -4,6 +4,7 @@ import { expect } from "chai";
 import {UserListItem} from "../../../../../main/admin/components/Users/List/UserListItem";
 import {mockUser} from "../../../../mocks/mockModels";
 import {User} from "../../../../../main/shared/models/Generated";
+import {InternalLink} from "../../../../../main/shared/components/InternalLink";
 
 describe("UserListItem", () => {
     it("can render", () => {
@@ -20,5 +21,17 @@ describe("UserListItem", () => {
         const lastLoggedIn = cells.at(3).text();
         expect(lastLoggedIn).to.eq("never")
     });
+
+    it("makes link", () => {
+
+        const user: User = mockUser();
+        user.last_logged_in = null;
+        user.username = "some.user";
+        const rendered = shallow(<UserListItem { ...user } />);
+        const link = rendered.find(InternalLink);
+
+        const href = link.prop("href");
+        expect(href).to.eq("/users/some-user/")
+    })
 
 });
