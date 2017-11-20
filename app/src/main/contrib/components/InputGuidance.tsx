2395b58c28890c3fad6b26e27c480b2d0ed0554f
kimwoodruff <k.woodruff@imperial.ac.uk>
2017-11-20 15:50:42 +0000

    Update InputGuidance.tsx
    Draft some text for this input guidance page (not yet complete)

--- app/src/main/contrib/components/InputGuidance.tsx
+++ app/src/main/contrib/components/InputGuidance.tsx
@@ -1,1 +1,42 @@
 
+import * as React from "react";
+import { PageWithHeaderAndNav } from "./PageWithHeader/PageWithHeaderAndNav";
+
+const commonStyles = require("../../shared/styles/common.css");
+
+export class InputGuidance extends PageWithHeaderAndNav<undefined> {
+    title() {
+        return <span>Guidance on model inputs: coverage and demographic data</span>;
+    }
+
+    renderPageContent() {
+        return <div>
+            <p>
+                Input data sources
+            </p>
+            <p>
+              Both coverage and demographic datasets to be used in the full model runs are available for download through Montagu. You must download and use these datasets, rather than your own datasets.
+            </p>
+            <p>
+                <ol>
+                    <li>
+                        list item 1
+                    </li>
+                    <li>
+                       lis item 2
+                    </li>
+                </ol>
+            </p>
+            <div className={ commonStyles.subSectionTitle }>Subsection title</div>
+            <p>
+                sub-section para 1
+            </p>
+            <p>
+                sub-section para 2
+            </p>
+            <p>
+               sub-section para 3
+            </p>
+        </div>;
+    }
+}
