fc6e667f9cc043dd4974707615c8413e929d828b
Alex Hill <alex.hill@gmail.com>
2017-11-23 16:36:29 +0000

    new branch with right naming convention

--- app/scripts/add-test-accounts-for-integration-tests.sh
+++ app/scripts/add-test-accounts-for-integration-tests.sh
@@ -16,5 +16,6 @@ $here/cli.sh addUserToGroup test.user test-group
 $here/cli.sh add "Report reviewer" report.reviewer \
     report.reviewer@example.com password \
     --if-not-exists
+
 $here/cli.sh addRole report.reviewer user
 $here/cli.sh addRole report.reviewer reports-reviewer
