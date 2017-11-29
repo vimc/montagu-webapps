7d2803fed87e6afe1b6d2aaabdedde1a87b85481
Yuri Kalinin <kalininj@gmail.com>
2017-11-29 11:11:59 +0000

    serve 5 static help pages as webpack asset in iframe
    Former-commit-id: 3d57b94a2c875183662c1ace8a4581eee8d84885

---
+++ app/src/main/contrib/components/Responsibilities/Guidance/Demographics/ResponsibilityGuidanceKosovo.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import ScrollableAnchor from 'react-scrollable-anchor';
+
+import {ContribPageWithHeader} from "../../../PageWithHeader/ContribPageWithHeader";
+import { ChooseGroupPage } from "../../../ChooseGroup/ChooseGroupPage";
+import { settings } from "../../../../../shared/Settings";
+import { InternalLink } from "../../../../../shared/components/InternalLink";
+
+const commonStyles = require("../../../../../shared/styles/common.css");
+
+const embeddedHtml = require('./html/kosovo.html');
+
+export class ResponsibilityGuidanceKosovo extends ContribPageWithHeader<undefined> {
+    name() {
+        return "Kosovo";
+    }
+
+    urlFragment() {
+        return "help/kosovo/";
+    }
+
+    title() {
+        return <span>Methods for creating Kosovo demographic data</span>;
+    }
+
+    hideTitle(): boolean {
+        return true;
+    }
+
+    parent() {
+        return new ChooseGroupPage();
+    }
+
+    renderPageContent() {
+        return <div>
+            <iframe src={embeddedHtml} width="100%" height="12231px" frameBorder="0"></iframe>
+        </div>;
+    }
+}
---
+++ app/src/main/contrib/components/Responsibilities/Guidance/Demographics/ResponsibilityGuidanceMarshallIslands.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import ScrollableAnchor from 'react-scrollable-anchor';
+
+import {ContribPageWithHeader} from "../../../PageWithHeader/ContribPageWithHeader";
+import { ChooseGroupPage } from "../../../ChooseGroup/ChooseGroupPage";
+import { settings } from "../../../../../shared/Settings";
+import { InternalLink } from "../../../../../shared/components/InternalLink";
+
+const commonStyles = require("../../../../../shared/styles/common.css");
+
+const embeddedHtml = require('./html/marshall.html');
+
+export class ResponsibilityGuidanceMarshallIslands extends ContribPageWithHeader<undefined> {
+    name() {
+        return "Marshall Islands";
+    }
+
+    urlFragment() {
+        return "help/marshall-islands/";
+    }
+
+    title() {
+        return <span>Methods for creating Marshall Islands demographic data</span>;
+    }
+
+    hideTitle(): boolean {
+        return true;
+    }
+
+    parent() {
+        return new ChooseGroupPage();
+    }
+
+    renderPageContent() {
+        return <div>
+            <iframe src={embeddedHtml} width="100%" height="9270px" frameBorder="0"></iframe>
+        </div>;
+    }
+}
