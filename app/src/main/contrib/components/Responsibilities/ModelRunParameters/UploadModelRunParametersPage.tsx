72a913306ead8461708b55fda5edd16ce50d50d0
Alex Hill <alex.hill@gmail.com>
2017-12-01 18:45:50 +0000

    even more tests

--- app/src/main/contrib/components/Responsibilities/ModelRunParameters/UploadModelRunParametersPage.tsx
+++ app/src/main/contrib/components/Responsibilities/ModelRunParameters/UploadModelRunParametersPage.tsx
@@ -29,7 +29,7 @@ export class UploadModelRunParametersPage extends ContribPageWithHeader<UploadMo
     }
 
     name() {
-        return "Upload burden estimates";
+        return "Upload model run parameters";
     }
 
     title() {
