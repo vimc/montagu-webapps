14bb3b115f2dce394173fdc2cd57a4a49638766e
Alex Hill <alex.hill@gmail.com>
2017-12-01 16:05:18 +0000

    more tests
    Former-commit-id: bee7ab92dc08cc0c7cfda5116a453b2f1582c79a

---
+++ app/src/test/contrib/components/Responsibilities/Uploads/UploadModelRunParameterFormTests.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import {expect} from "chai";
+import {shallow, ShallowWrapper} from "enzyme";
+import {Sandbox} from "../../../../Sandbox";
+import {mockFetcher} from "../../../../mocks/mockRemote";
+import {UploadModelRunParametersForm} from "../../../../../main/contrib/components/Responsibilities/ModelRunParameters/ModelRunParametersUploadForm";
+import {UploadForm} from "../../../../../main/shared/components/UploadForm";
+
+describe('UploadModelRunParametersForm', () => {
+    let rendered: ShallowWrapper<any, any>;
+    const sandbox = new Sandbox();
+    before(() => mockFetcher(Promise.resolve(null)));
+
+    afterEach(() => sandbox.restore());
+
+    it("renders UploadForm", () => {
+
+        rendered = shallow(<UploadModelRunParametersForm
+            token={"token"}
+            groupId={"group-1"}
+            touchstoneId={"touchstone-id"}
+            diseases={["d1"]}
+        />);
+
+        const form = rendered.find(UploadForm);
+        expect(form).to.have.lengthOf(1);
+
+    });
+
+    it("renders disease select input", () => {
+
+        rendered = shallow(<UploadModelRunParametersForm
+            token={"token"}
+            groupId={"group-1"}
+            touchstoneId={"touchstone-id"}
+            diseases={["d1"]}
+        />);
+
+        const select = rendered.find('select[name="disease"]');
+        expect(select).to.have.lengthOf(1);
+
+        const options = select.find("option");
+        expect(options).to.have.lengthOf(2);
+
+        expect(options.first().text()).to.equal("-- Select a disease --");
+        expect(options.first().prop("value")).to.be.empty;
+
+        expect(options.last().text()).to.equal("d1");
+        expect(options.last().prop("value")).to.equal("d1");
+
+    });
+
+    it("renders description text input", () => {
+
+        rendered = shallow(<UploadModelRunParametersForm
+            token={"token"}
+            groupId={"group-1"}
+            touchstoneId={"touchstone-id"}
+            diseases={["d1"]}
+        />);
+
+        const input = rendered.find('input[name="description"]');
+        expect(input).to.have.lengthOf(1);
+    });
+
+    it("enable submit is false if no description", () => {
+
+        rendered = shallow(<UploadModelRunParametersForm
+            token={"token"}
+            groupId={"group-1"}
+            touchstoneId={"touchstone-id"}
+            diseases={["d1"]}
+        />);
+
+        const form = rendered.find(UploadForm);
+        rendered.setState({"diseaseSelected": true});
+
+        expect(form.prop("enableSubmit")).to.be.false;
+    });
+
+    it("enable submit is false if no disease", () => {
+
+        rendered = shallow(<UploadModelRunParametersForm
+            token={"token"}
+            groupId={"group-1"}
+            touchstoneId={"touchstone-id"}
+            diseases={["d1"]}
+        />);
+
+        const form = rendered.find(UploadForm);
+        rendered.setState({"descriptionSelected": true});
+
+        expect(form.prop("enableSubmit")).to.be.false;
+    });
+
+
+    it("enable submit is true if disease and description selected", () => {
+
+        rendered = shallow(<UploadModelRunParametersForm
+            token={"token"}
+            groupId={"group-1"}
+            touchstoneId={"touchstone-id"}
+            diseases={["d1"]}
+        />);
+
+        const form = rendered.find(UploadForm);
+        rendered.setState({"descriptionSelected": true, "diseaseSelected": true});
+
+        expect(form.prop("enableSubmit")).to.be.false;
+    });
+
+
+});
