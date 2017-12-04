3b2c2045006f63eab7c9185c0dd3e892930bda3a
Yuri Kalinin <kalininj@gmail.com>
2017-12-04 11:08:00 +0000

    chenged to show id in breadcrumb for choose action page and download coverage page

--- app/src/main/contrib/components/Responsibilities/Coverage/DownloadCoveragePage.tsx
+++ app/src/main/contrib/components/Responsibilities/Coverage/DownloadCoveragePage.tsx
@@ -31,7 +31,8 @@ export class DownloadCoveragePage extends ContribPageWithHeader<LocationProps> {
     }
 
     name() {
-        return "Download coverage data";
+        const s = responsibilityStore.getState();
+        return s.currentResponsibility.scenario.id;
     }
 
     title() {
