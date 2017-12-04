7b2c044e9bc691aa98621cf4cb8190586bb948ef
Alex Hill <alex.hill@gmail.com>
2017-12-04 11:04:07 +0000

    revert whitesapce for cleaner diff

--- app/src/main/contrib/components/Responsibilities/Coverage/DownloadCoverageContent.tsx
+++ app/src/main/contrib/components/Responsibilities/Coverage/DownloadCoverageContent.tsx
@@ -28,7 +28,6 @@ interface Props extends HasFormatOption {
 }
 
 export class DownloadCoverageContentComponent extends RemoteContentComponent<DownloadCoverageComponentProps> {
-
     static getStores() {
         return [responsibilityStore];
     }
