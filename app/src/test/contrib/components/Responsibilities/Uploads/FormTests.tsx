3a4420fe70e21e929cc0e24f1229944cbdaaa609
Alex Hill <alex.hill@gmail.com>
2017-12-08 18:29:31 +0000

    more tests

--- app/src/test/contrib/components/Responsibilities/Uploads/FormTests.tsx
+++ app/src/test/contrib/components/Responsibilities/Uploads/FormTests.tsx
@@ -1,16 +1,25 @@
 import * as React from "react";
 import {expect} from "chai";
-import {mount, render, shallow} from "enzyme";
+import {mount, shallow} from "enzyme";
 import {Sandbox} from "../../../../Sandbox";
-import {CreateBurdenEstimateSetForm} from "../../../../../main/contrib/components/Responsibilities/BurdenEstimates/CreateBurdenEstimateSetForm";
 import {Alert} from "../../../../../main/shared/components/Alert";
-import {ReactElement} from "react";
-import {OptionSelector} from "../../../../../../../app/src/main/contrib/components/OptionSelector/OptionSelector";
-import {Form} from "../../../../../../../app/src/main/contrib/components/Responsibilities/BurdenEstimates/Form";
-import {Result} from "../../../../../../../app/src/main/shared/models/Generated";
+import {Form} from "../../../../../main/contrib/components/Responsibilities/BurdenEstimates/Form";
+import {Result} from "../../../../../main/shared/models/Generated";
+import {mockFetcherResponse, mockResponse} from "../../../../mocks/mockRemote";
+import {checkAsync} from "../../../../testHelpers";
 
 describe("CreateEstimatesFormComponent", () => {
     const sandbox = new Sandbox();
+
+    const props = {
+        url: "url",
+        successCallback: (result: Result) => {
+        },
+        buildPostData: (formData: FormData) => {
+        },
+        successMessage: "success message",
+        submitText: "submit"
+    };
 
     afterEach(() => {
         sandbox.restore();
@@ -18,60 +27,40 @@ describe("CreateEstimatesFormComponent", () => {
 
     it("renders children", () => {
 
+        const rendered = mount(<Form {...props}><span id="test">hi</span></Form>);
+        expect(rendered.find("#test").text()).to.eq("hi");
     });
 
-    it("shows errors", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
+    it("passes errors to alert", () => {
 
-        const rendered = shallow(<Form {...props} />);
+        const rendered = mount(<Form {...props} />);
         const errors = [{code: "err", message: "err message"}];
 
         rendered.setState({errors: errors});
-        rendered.update();
 
-        expect(rendered.find(Alert).at(0).prop("hasError")).to.eq(true);
-        expect(rendered.find(Alert).at(0).prop("hasSuccess")).to.eq(false);
+        const alert = rendered.find(Alert).at(0);
+
+        expect(alert.prop("hasError")).to.eq(true);
+        expect(alert.prop("hasSuccess")).to.eq(false);
+        expect(alert.prop("message")).to.eq("err message");
     });
 
-    it("shows success", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
+    it("passes success to alert", () => {
 
-        const rendered = shallow(<Form {...props} />);
+        const rendered = mount(<Form {...props} />);
         rendered.setState({hasSuccess: true});
 
-        expect(rendered.find(Alert).first().prop("hasSuccess")).to.eq(true);
-        expect(rendered.find(Alert).first().prop("hasError")).to.eq(false);
+        const alert = rendered.find(Alert).at(0);
+
+        expect(alert.prop("hasSuccess")).to.eq(true);
+        expect(alert.prop("hasError")).to.eq(false);
+        expect(alert.prop("message")).to.eq("success message");
     });
 
     it("result callback sets successful state", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
 
         const form = new Form(props);
-        const spy = sandbox.setSpy(form, "setState");
+        const spy = sandbox.setStub(form, "setState");
 
         form.resultCallback({status: "success", errors: [], data: null});
 
@@ -82,18 +71,25 @@ describe("CreateEstimatesFormComponent", () => {
         })).to.eq(true);
     });
 
+    it("calls result callback", (done) => {
+
+        mockFetcherResponse();
+        const form = new Form(props);
+        sandbox.setStub(form, "setState");
+
+        const spy = sandbox.setStub(form, "resultCallback");
+        form.submitForm({test: "test"});
+
+        checkAsync(done, (afterWait) => {
+            afterWait(done, () => {
+                expect(spy.called).to.eq(true);
+            })})
+    });
+
     it("calls success callback if result is successful", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
 
         const form = new Form(props);
+        sandbox.setStub(form, "setState");
         const spy = sandbox.setSpy(props, "successCallback");
 
         form.resultCallback({status: "success", errors: [], data: null});
@@ -102,17 +98,9 @@ describe("CreateEstimatesFormComponent", () => {
     });
 
     it("does not call success callback if result is unsuccessful", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
 
         const form = new Form(props);
+        sandbox.setStub(form, "setState");
         const spy = sandbox.setSpy(props, "successCallback");
 
         form.resultCallback({status: "failure", errors: [{code: "e1", message: "error one"}], data: null});
@@ -121,19 +109,10 @@ describe("CreateEstimatesFormComponent", () => {
     });
 
     it("result callback sets error state", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
-git s
+
         const form = new Form(props);
 
-        const spy = sandbox.setSpy(form, "setState");
+        const spy = sandbox.setStub(form, "setState");
 
         const errors = [{code: "e1", message: "error one"}];
         form.resultCallback({status: "failure", errors: errors, data: null});
@@ -146,51 +125,67 @@ git s
     });
 
     it("does not submit if validate fails", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
 
         const form = new Form(props);
         const spy = sandbox.setSpy(form, "submitForm");
+        sandbox.setStub(form, "setState");
         sandbox.setStub(form, "getData").returns("test");
 
         const formElement = {checkValidity: () => false};
 
-        form.onSubmit(Object.assign({ target: formElement, preventDefault: () => {} }));
+        form.onSubmit(Object.assign({
+            target: formElement, preventDefault: () => {
+            }
+        }));
         expect(spy.called).to.eq(false);
 
     });
 
-
     it("submits if validate passes", () => {
-        const props = {
-            url: "url",
-            successCallback: (result: Result) => {
-            },
-            buildPostData: (formData: FormData) => {
-            },
-            successMessage: "success",
-            submitText: "submit"
-        };
 
         const form = new Form(props);
         const spy = sandbox.setSpy(form, "submitForm");
+        sandbox.setStub(form, "setState");
         sandbox.setStub(form, "getData").returns("test");
 
         const formElement = {checkValidity: () => true};
 
-        form.onSubmit(Object.assign({ target: formElement, preventDefault: () => {} }));
+        form.onSubmit(Object.assign({
+            target: formElement, preventDefault: () => {
+            }
+        }));
         expect(spy.calledWith("test")).to.eq(true);
 
     });
 
+    it("sets state to validated after submit", () => {
 
+        const form = new Form(props);
+        sandbox.setStub(form, "submitForm");
+        sandbox.setStub(form, "getData").returns("test");
+        const spy = sandbox.setStub(form, "setState");
 
+        const formElement = {checkValidity: () => true};
+
+        form.onSubmit(Object.assign({
+            target: formElement, preventDefault: () => {
+            }
+        }));
+
+        expect(spy.calledWith({validated: true})).to.eq(true);
+
+    });
+
+    it("adds was-validated class to form", () => {
+
+        const rendered = mount(<Form {...props} />);
+
+        expect(rendered.find("form").hasClass("was-validated")).to.eq(false);
+
+        rendered.setState({validated: true});
+
+        expect(rendered.find("form").hasClass("was-validated")).to.eq(true);
+
+    });
 
 });
