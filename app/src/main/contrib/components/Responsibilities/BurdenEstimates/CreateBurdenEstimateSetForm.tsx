99cc09439303d451b3c04ef3bbbec6d8c3961ee3
Alex Hill <alex.hill@gmail.com>
2017-12-11 10:27:10 +0000

    pass input data through to form

--- app/src/main/contrib/components/Responsibilities/BurdenEstimates/CreateBurdenEstimateSetForm.tsx
+++ app/src/main/contrib/components/Responsibilities/BurdenEstimates/CreateBurdenEstimateSetForm.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import {responsibilityStore} from "../../../stores/ResponsibilityStore";
 import {OptionSelector} from "../../OptionSelector/OptionSelector";
 import {Form} from "./Form";
+import {BurdenEstimateSetTypeCode} from "../../../../shared/models/Generated";
+import {FormEvent} from "react";
 
 interface BurdenEstimateProps {
     groupId: string;
@@ -9,19 +11,46 @@ interface BurdenEstimateProps {
     scenarioId: string;
 }
 
-export class CreateBurdenEstimateSetForm extends React.Component<BurdenEstimateProps, undefined> {
+interface CreateBurdenEstimateSet {
+    type: { type: BurdenEstimateSetTypeCode, details: string }
+}
+
+interface FormData {
+    data: CreateBurdenEstimateSet
+}
+
+export class CreateBurdenEstimateSetForm extends React.Component<BurdenEstimateProps, FormData> {
+
+    constructor() {
+        super();
+        this.state = {
+            data: {
+                type: {
+                    type: null,
+                    details: null
+                }
+            }
+        }
+    }
 
     static successCallback() {
         responsibilityStore.refreshResponsibilities()
     }
 
-    static buildPostData(formData: FormData) {
-        return {
-            type: {
-                type: formData.get("typeCode"),
-                details: formData.get("details")
-            }
-        }
+    onTypeChange(e: FormEvent<HTMLInputElement>) {
+        const newData = this.state.data;
+        newData.type.type = (e.target as HTMLInputElement).value as BurdenEstimateSetTypeCode;
+        this.setState({
+            data: newData
+        })
+    }
+
+    onDetailsChange(e: FormEvent<HTMLInputElement>) {
+        const newData = this.state.data;
+        newData.type.details = (e.target as HTMLInputElement).value;
+        this.setState({
+            data: newData
+        })
     }
 
     render() {
@@ -37,16 +66,15 @@ export class CreateBurdenEstimateSetForm extends React.Component<BurdenEstimateP
             <h4>Create a new set of burden estimates:</h4>
             <Form successCallback={CreateBurdenEstimateSetForm.successCallback}
                   url={`/modelling-groups/${this.props.groupId}/responsibilities/${this.props.touchstoneId}/${this.props.scenarioId}/estimate-sets/`}
-                  buildPostData={CreateBurdenEstimateSetForm.buildPostData}
                   successMessage={successMessage}
-                  submitText={"Create"}>
+                  submitText={"Create"}
+                  data={this.state.data}>
                 <div className="row">
                     <div className="col">
                         <label>How were these estimates calculated?</label>
                         <OptionSelector name={"typeCode"}
                                         defaultOption={"-- Please select one --"}
-                                        options={options} onChange={() => {
-                        }}
+                                        options={options} onChange={this.onTypeChange.bind(this)}
                                         className="form-control" required={true}/>
                         <div className="invalid-feedback">
                             Please tell us how these estimates were calculated
@@ -54,7 +82,8 @@ export class CreateBurdenEstimateSetForm extends React.Component<BurdenEstimateP
                     </div>
                     <div className="col">
                         <label>Details of this run</label>
-                        <input type="text" className={"form-control"} name="details"/>
+                        <input type="text" className={"form-control"} name="details"
+                               onChange={this.onDetailsChange.bind(this)}/>
                     </div>
                 </div>
             </Form>
