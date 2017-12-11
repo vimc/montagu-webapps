f75d57f1a197a72cc57a0e4fef1255d7927d191b
Alex Hill <alex.hill@gmail.com>
2017-12-11 12:22:05 +0000

    onchange handler for option selectorfix
    Former-commit-id: 644360d8a36055fd5ef80ed133aa98ac83e8e804

--- app/src/integrationTests/ContribIntegrationTests.tsx
+++ app/src/integrationTests/ContribIntegrationTests.tsx
@@ -283,14 +283,17 @@ class ContributionPortalIntegrationTests extends IntegrationTestSuite {
             const rendered = mount(<CreateBurdenEstimateSetForm
                 touchstoneId={touchstoneId} groupId={groupId} scenarioId={scenarioId}/>);
 
-            rendered.find('input["name=details"]').simulate("change", "central-averaged");
+            rendered.find('input[name="details"]').simulate("change", "central-averaged");
 
             // mutate underlying node, as the simulate event doesn't pass through args as expected on
             // select elements
             // https://github.com/airbnb/enzyme/issues/38
-            (rendered.find('select["name=typeCode"]').getNode() as any).selectedIndex = 2;
+            (rendered.find('select[name="typeCode"]').getNode() as any).selectedIndex = 2;
+
+            console.log(rendered.find(Form).props());
 
             const form = new Form(rendered.find(Form).props());
+
             const spy = new Sandbox().setSpy(form, "resultCallback");
 
             form.submitForm();
