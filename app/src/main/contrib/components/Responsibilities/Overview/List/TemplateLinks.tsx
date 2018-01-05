7e56d090b01903a13e3b72ea2bf922c24126c219
Alex Hill <alex.hill@gmail.com>
2018-01-05 13:47:17 +0000

    cleaner logic
    Former-commit-id: a02d8d5c12959d0d63af0a70d15250182f55e509

--- app/src/main/contrib/components/Responsibilities/Overview/List/TemplateLinks.tsx
+++ app/src/main/contrib/components/Responsibilities/Overview/List/TemplateLinks.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {mainStore} from "../../../../stores/MainStore";
 import {ExtendedResponsibility} from "../../../../models/ResponsibilitySet";
-import { settings } from "../../../../../shared/Settings";
+import {settings} from "../../../../../shared/Settings";
 
 const stochasticParams = require('./stochastic_template_params.csv');
 
@@ -32,97 +32,105 @@ export class TemplateLink extends React.Component<TemplateLinkProps, undefined> 
                         estimates</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}98-countries-stochastic_burden_template_HepB-Li.csv`}>98 countries stochastic
+                    <a href={`${templatePath}98-countries-stochastic_burden_template_HepB-Li.csv`}>98 countries
+                        stochastic
                         estimates</a>
                 </li>
             </ul>
         }
 
-        if (this.props.groupId.toLowerCase() == "cda-razavi"){
+        if (this.props.groupId.toLowerCase() == "cda-razavi") {
             return <ul className="list-unstyled">
                 <li>
-                    <a href={`${templatePath}98-countries-central_burden_template_HepB-CDA-Razavi.csv`}>all 98 countries - central</a>
+                    <a href={`${templatePath}98-countries-central_burden_template_HepB-CDA-Razavi.csv`}>all 98 countries
+                        - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}98-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>all 98 countries - stochastic</a>
+                    <a href={`${templatePath}98-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>all 98
+                        countries - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}19-countries-central_burden_template_HepB-CDA-Razavi.csv`}>non-gavi with current BD_facility - central</a>
+                    <a href={`${templatePath}19-countries-central_burden_template_HepB-CDA-Razavi.csv`}>non-gavi with
+                        current BD_facility - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}19-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>non-gavi with current BD_facility - stochastic</a>
+                    <a href={`${templatePath}19-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>non-gavi with
+                        current BD_facility - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}79-countries-central_burden_template_HepB-CDA-Razavi.csv`}>gavi + non-gavi without BD_facility - central</a>
+                    <a href={`${templatePath}79-countries-central_burden_template_HepB-CDA-Razavi.csv`}>gavi + non-gavi
+                        without BD_facility - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}79-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>gavi + non-gavi without BD_facility - stochastic</a>
+                    <a href={`${templatePath}79-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>gavi +
+                        non-gavi without BD_facility - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}92-countries-central_burden_template_HepB-CDA-Razavi.csv`}>gavi + non-gavi with current BD_facility - central</a>
+                    <a href={`${templatePath}92-countries-central_burden_template_HepB-CDA-Razavi.csv`}>gavi + non-gavi
+                        with current BD_facility - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}92-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>gavi + non-gavi with current BD_facility - stochastic</a>
+                    <a href={`${templatePath}92-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>gavi +
+                        non-gavi with current BD_facility - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}73-countries-central_burden_template_HepB-CDA-Razavi.csv`}>gavi countries - central</a>
+                    <a href={`${templatePath}73-countries-central_burden_template_HepB-CDA-Razavi.csv`}>gavi countries -
+                        central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}73-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>gavi countries - stochastic</a>
+                    <a href={`${templatePath}73-countries-stochastic_burden_template_HepB-CDA-Razavi.csv`}>gavi
+                        countries - stochastic</a>
                 </li>
             </ul>
         }
 
-        if (this.props.groupId.toLowerCase() == "ic-hallett"){
+        if (this.props.groupId.toLowerCase() == "ic-hallett") {
             return <ul className="list-unstyled">
                 <li>
-                    <a href={`${templatePath}98-countries-central_burden_template_HepB-IC-Hallett.csv`}>all 98 countries - central</a>
+                    <a href={`${templatePath}98-countries-central_burden_template_HepB-IC-Hallett.csv`}>all 98 countries
+                        - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}98-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>all 98 countries - stochastic</a>
+                    <a href={`${templatePath}98-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>all 98
+                        countries - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}19-countries-central_burden_template_HepB-IC-Hallett.csv`}>non-gavi with current BD_facility - central</a>
+                    <a href={`${templatePath}19-countries-central_burden_template_HepB-IC-Hallett.csv`}>non-gavi with
+                        current BD_facility - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}19-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>non-gavi with current BD_facility - stochastic</a>
+                    <a href={`${templatePath}19-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>non-gavi with
+                        current BD_facility - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}79-countries-central_burden_template_HepB-IC-Hallett.csv`}>gavi + non-gavi without BD_facility - central</a>
+                    <a href={`${templatePath}79-countries-central_burden_template_HepB-IC-Hallett.csv`}>gavi + non-gavi
+                        without BD_facility - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}79-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>gavi + non-gavi without BD_facility - stochastic</a>
+                    <a href={`${templatePath}79-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>gavi +
+                        non-gavi without BD_facility - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}92-countries-central_burden_template_HepB-IC-Hallett.csv`}>gavi + non-gavi with current BD_facility - central</a>
+                    <a href={`${templatePath}92-countries-central_burden_template_HepB-IC-Hallett.csv`}>gavi + non-gavi
+                        with current BD_facility - central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}92-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>gavi + non-gavi with current BD_facility - stochastic</a>
+                    <a href={`${templatePath}92-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>gavi +
+                        non-gavi with current BD_facility - stochastic</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}73-countries-central_burden_template_HepB-IC-Hallett.csv`}>gavi countries - central</a>
+                    <a href={`${templatePath}73-countries-central_burden_template_HepB-IC-Hallett.csv`}>gavi countries -
+                        central</a>
                 </li>
                 <li>
-                    <a href={`${templatePath}73-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>gavi countries - stochastic</a>
+                    <a href={`${templatePath}73-countries-stochastic_burden_template_HepB-IC-Hallett.csv`}>gavi
+                        countries - stochastic</a>
                 </li>
             </ul>
         }
 
-        return <div>
-            {this.props.touchstoneId != settings.modellerApplicantsTouchstoneId &&
-            <div>
-                <a key={`${disease.id}-d`}
-                   href={href}>{disease.name} - central</a>
-            </div>
-            }
-            {this.props.touchstoneId != settings.modellerApplicantsTouchstoneId &&
-                <div>
-                    <a key={`${disease.id}-s`}
-                       href={hrefStochastic}>{disease.name} - stochastic</a>
-                </div>
-            }
-            {this.props.touchstoneId == settings.modellerApplicantsTouchstoneId &&
-            <div>
+        if (this.props.touchstoneId != settings.modellerApplicantsTouchstoneId) {
+            return <div>
                 <a key="central_burden_template_Rubella-generic.csv"
                    href={`${templatePath}central_burden_template_Rubella-generic.csv`}>Rubella</a>
                 <a key="central_burden_template_JE-generic.csv"
@@ -130,9 +138,19 @@ export class TemplateLink extends React.Component<TemplateLinkProps, undefined> 
                 <a key="central_burden_template_YF-generic.csv"
                    href={`${templatePath}central_burden_template_YF-generic.csv`}>Yellow Fever</a>
             </div>
-            }
+        }
 
-        </div>;
+        else
+            return <div>
+                <div>
+                    <a key={`${disease.id}-d`}
+                       href={href}>{disease.name} - central</a>
+                </div>
+                <div>
+                    <a key={`${disease.id}-s`}
+                       href={hrefStochastic}>{disease.name} - stochastic</a>
+                </div>
+            </div>;
     }
 }
 
@@ -154,10 +172,10 @@ export class TemplateLinks extends React.Component<TemplateLinksProps, undefined
             return <div>Download burden estimate templates:<br/>
                 {links}
                 {this.props.touchstoneId != settings.modellerApplicantsTouchstoneId &&
-                    <div>
-                        <a key={"params"}
-                           href={stochasticParams}>Stochastic parameters template</a>
-                    </div>
+                <div>
+                    <a key={"params"}
+                       href={stochasticParams}>Stochastic parameters template</a>
+                </div>
                 }
             </div>;
         } else {
