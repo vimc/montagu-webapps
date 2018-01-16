387fee856567e0d76d0ada2f7132b9f961f8153f
Alex Hill <alex.hill@gmail.com>
2018-01-16 13:00:13 +0000

    return null token if current estimate set is null
    Former-commit-id: 9ceabdcfef51a9edfa1a6003435684864d67ff93

--- app/src/test/contrib/fetch/EstimatesTokenFetchTests.ts
+++ app/src/test/contrib/fetch/EstimatesTokenFetchTests.ts
@@ -5,8 +5,9 @@ import {
 import {FetchHelper} from "../../shared/fetch/helpers";
 import {responsibilityStore} from "../../../main/contrib/stores/ResponsibilityStore";
 import {alt} from "../../../main/shared/alt";
+import {expect} from "chai"
 
-describe("ResponsibilityStore.fetchOneTimeEstimatesToken", () => {
+describe("ResponsibilityStore.fetchOneTimeEstimatesToken with redirect path", () => {
     const group = mockModellingGroup({id: "group-id"});
     const touchstone = mockTouchstone({id: "touchstone-id"});
     const responsibility = mockResponsibility({current_estimate_set: mockBurdenEstimateSet({id: 123})},
@@ -29,4 +30,39 @@ describe("ResponsibilityStore.fetchOneTimeEstimatesToken", () => {
             triggerFetch: () => responsibilityStore.fetchOneTimeEstimatesToken(),
             makePayload: () => "TOKEN"
         }).addTestsToMocha();
+});
+
+describe("ResponsibilityStore.fetchOneTimeEstimatesToken without redirect path", () => {
+    const group = mockModellingGroup({id: "group-id"});
+    const touchstone = mockTouchstone({id: "touchstone-id"});
+    const responsibility = mockResponsibility({current_estimate_set: mockBurdenEstimateSet({id: 123})},
+        mockScenario({id: "scenario-id"}));
+
+    new FetchHelper<string, string>(
+        {
+            expectedURL:
+                "/modelling-groups/group-id/responsibilities/touchstone-id/scenario-id/estimate-sets/123/get_onetime_link/",
+            prepareForFetch: () => {
+                alt.bootstrap(JSON.stringify({
+                    ResponsibilityStore: {
+                        currentTouchstone: touchstone,
+                        currentModellingGroup: group,
+                        currentResponsibility: responsibility,
+                        redirectPath: null
+                    }
+                }));
+            },
+            triggerFetch: () => responsibilityStore.fetchOneTimeEstimatesToken(),
+            makePayload: () => "TOKEN"
+        }).addTestsToMocha();
+});
+
+describe("ResponsibilityStore.fetchOneTimeEstimatesToken without current estimate set", () => {
+    const group = mockModellingGroup({id: "group-id"});
+    const touchstone = mockTouchstone({id: "touchstone-id"});
+    const responsibility = mockResponsibility({}, mockScenario({id: "scenario-id"}));
+
+    responsibilityStore.fetchOneTimeEstimatesToken().then((token)=>{
+        expect(token).to.null;
+    });
 });
