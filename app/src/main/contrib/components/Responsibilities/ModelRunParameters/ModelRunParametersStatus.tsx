614ab165f25ebabb44eecb949841aeeb5156f72b
Yuri Kalinin <kalininj@gmail.com>
2018-01-23 15:16:56 +0000

    signel token in local state and warning fix

--- app/src/main/contrib/components/Responsibilities/ModelRunParameters/ModelRunParametersStatus.tsx
+++ app/src/main/contrib/components/Responsibilities/ModelRunParameters/ModelRunParametersStatus.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 interface State {
     set: ModelRunParameterSet,
-    tokens: TokensMap,
+    token: string,
     groupId: string,
     touchstoneId: string
 }
@@ -22,15 +22,15 @@ interface State {
 export class ModelRunParametersStatus extends React.Component<Props, State> {
 
     constructor(props: Props) {
-        super();
-        const storeState = runParametersStore.getState();
+        super(props);
         this.state = {
-            set: this.getLastSetForDisease(storeState.parameterSets, props.disease),
-            tokens: storeState.oneTimeTokens,
-            groupId: storeState.groupId,
-            touchstoneId: storeState.touchstoneId
+            set: null,
+            token: null,
+            groupId: "",
+            touchstoneId: ""
         }
         this.refreshToken = this.refreshToken.bind(this)
+        this.onChange = this.onChange.bind(this)
     }
 
     getLastSetForDisease(allSets: ModelRunParameterSet[], disease: string) {
@@ -43,19 +43,41 @@ export class ModelRunParametersStatus extends React.Component<Props, State> {
         return null;
     }
 
+    getTokenBySet(set: ModelRunParameterSet, tokens: TokensMap) {
+        if (!set) return null;
+        if (tokens[set.id]) return tokens[set.id];
+        return null;
+    }
+
     componentDidMount() {
-        runParametersStore.listen(this.onChange.bind(this));
+        const s = runParametersStore.getState();
+        const set = this.getLastSetForDisease(s.parameterSets, this.props.disease);
+        const token = this.getTokenBySet(set, s.oneTimeTokens);
+        this.setState({
+            set: set,
+            token: token,
+            groupId: s.groupId,
+            touchstoneId: s.touchstoneId
+        });
+        runParametersStore.listen(this.onChange);
         if (this.state.set) {
             runParameterActions.fetchToken(this.state.groupId, this.state.touchstoneId, this.state.set.id)
         }
     }
 
+    componentWillUnmount() {
+        runParametersStore.unlisten(this.onChange);
+    }
+
     onChange(storeState: RunParametersState) {
         const oldSetId = this.state.set ? this.state.set.id : null;
+        const set = this.getLastSetForDisease(storeState.parameterSets, this.props.disease);
         this.setState({
-            set: this.getLastSetForDisease(storeState.parameterSets, this.props.disease),
-            tokens: storeState.oneTimeTokens
-        })
+            set: set,
+            token: this.getTokenBySet(set, storeState.oneTimeTokens),
+            groupId: storeState.groupId,
+            touchstoneId: storeState.touchstoneId
+        });
         const newSetId = this.state.set ? this.state.set.id : null;
         if (newSetId && oldSetId != newSetId) {
             runParameterActions.fetchToken(storeState.groupId, storeState.touchstoneId, newSetId)
@@ -80,9 +102,9 @@ export class ModelRunParametersStatus extends React.Component<Props, State> {
 
             // TODO add link when API endpoint implemented
             const downloadParamsLink = <OneTimeButton
-                token={this.state.tokens[set.id]}
+                token={this.state.token}
                 refreshToken={this.refreshToken(set.id)}
-                enabled={!!this.state.tokens[set.id]}
+                enabled={!!this.state.token}
                 element="Link"
             >
                 Download data set
