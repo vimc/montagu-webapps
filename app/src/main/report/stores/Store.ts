8851935789f6e665a364b354d5eed1f9e11d8908
Yuri Kalinin <kalininj@gmail.com>
2018-01-29 14:45:18 +0000

    services as middleware
    Former-commit-id: 395b92b2b69a7ec8f39f05d07f85741cf1224428

--- app/src/main/report/stores/Store.ts
+++ app/src/main/report/stores/Store.ts
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from '../reducers/Reducers';
+import { authService } from '../../shared/services/AuthService';
 
 const store = createStore(
     reducers,
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, authService),
 );
 
 export default store;
