d470c5ac1e60e6633a96a3fbb11a0e38115798d2
Yuri Kalinin <kalininj@gmail.com>
2018-02-01 22:46:48 +0000

    cleanup  and introduction of await async

--- app/src/main/contrib/actions/ModellingGroupsActions.ts
+++ app/src/main/contrib/actions/ModellingGroupsActions.ts
@@ -6,17 +6,18 @@ import { modellingGroupsService } from "../services/modellingGroupsService";
 export const ModellingGroupsActions = {
 
     getGroups() {
-        return (dispatch: Dispatch<any>, getState: Function) => {
-            modellingGroupsService(getState().auth.bearerToken).getGroups()
-                .then((response: AxiosResponse) => {
-                    const userGroups = getState().auth.modellingGroups;
-                    console.log('bf comp', userGroups, response.data.data)
-                    const groups = response.data.data.filter((item: any) => userGroups.indexOf(item.id) > -1)
-                    dispatch({
-                        type: 'GROUPS_FETCHED',
-                        data: groups
-                    });
+        return async (dispatch: Dispatch<any>, getState: Function) => {
+            try {
+                const response = await modellingGroupsService(getState).getGroups()
+                const userGroups = getState().auth.modellingGroups;
+                const groups = response.data.data.filter((item: any) => userGroups.indexOf(item.id) > -1)
+                dispatch({
+                    type: 'GROUPS_FETCHED',
+                    data: groups
                 });
+            } catch(e) {
+                console.log("Could not fetch modelling groups for a user");
+            }
         }
     },
 
--- app/src/main/contrib/stores/createContribStore.ts
+++ app/src/main/contrib/stores/createContribStore.ts
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import { dispatchInReducerMiddleware } from "../../shared/modules/dispatchInReducerMiddleware";
 import reducers from '../reducers/reducers';
 
 export function createReportStore() {
     return createStore(
         reducers,
-        applyMiddleware(thunk, dispatchInReducerMiddleware),
+        applyMiddleware(thunk),
     );
 }
 
