ea2d9243abe6f3a6dac2c097fb15e88d3eff947c
Alex Hill <alex.hill@gmail.com>
2018-02-05 18:47:12 +0000

    latest changes
    Former-commit-id: 11b97073b179dd686d1092d21ebebcad3d1808b6

--- app/src/main/report/components/MainMenu/MainMenu.tsx
+++ app/src/main/report/components/MainMenu/MainMenu.tsx
@@ -36,28 +36,24 @@ export class MainMenu extends ReportingPageWithHeader<any> {
 
     render(): JSX.Element {
         return <Page page={this}>
+            <div>
+                <h4>Find a report:</h4>
+                <Typeahead minLength={2} options={["DALYs Guidance",
+                    "coverage data from 201708test-1",
+                    "coverage data from 201708test-2",
+                    "burden estimate template tables",
+                    "Create scenario-specific burden estimate templates",
+                    "open call burden estimate templates",
+                    "Discussion and methods regarding child mortality rates",
+                    "Methods for creating Kosovo demographic data",
+                    "Methods for creating Marshsall Islands demographic data"
+                ]}/>
+                <a href="#">View all</a>
+            </div>
             <div className="container">
+
                 <div className="row">
 
-                    <div className="col-12 col-lg-6">
-                        <div className="jumbotron" style={{marginTop: "100px"}}>
-                            <h2 className="display-4">Find a report</h2>
-                            <p className="lead">
-                                Start typing the name of a report to search
-                            </p>
-                            <Typeahead minLength={2} options={["DALYs Guidance",
-                                "coverage data from 201708test-1",
-                                "coverage data from 201708test-2",
-                                "burden estimate template tables",
-                                "Create scenario-specific burden estimate templates",
-                                "open call burden estimate templates",
-                                "Discussion and methods regarding child mortality rates",
-                                "Methods for creating Kosovo demographic data",
-                                "Methods for creating Marshsall Islands demographic data"
-                            ]}/>
-                            <a href="#">View all</a>
-                        </div>
-                    </div>
 
                     <div className="col-12 col-lg-6">
                         <div className="mb-3 mr-5 ml-5 mt-3">
@@ -65,6 +61,7 @@ export class MainMenu extends ReportingPageWithHeader<any> {
                         <img src={testimage} className="img-fluid"/>
                         </div>
                     </div>
+
 
                     <div className="col-12 col-sm-6 col-lg-4 mt-3">
                         <div className="">
@@ -74,10 +71,10 @@ export class MainMenu extends ReportingPageWithHeader<any> {
                                     <div className="card-header arrow-up">Guidance</div>
                                     <Collapse isOpen={true}>
                                             <div className="card-body p-1">
-                                                <ul className="list-group list-group-flush">
-                                                    <li className="list-group-item"><a href="#">DALYs Guidance</a></li>
-                                                    <li className="list-group-item"><a href="#">Small countries guidance</a></li>
-                                                    <li className="list-group-item"><a href="#">Coverage guidance</a></li>
+                                                <ul className="list-unstyled">
+                                                    <li className=" "><a href="#">DALYs Guidance</a></li>
+                                                    <li className=" "><a href="#">Small countries guidance</a></li>
+                                                    <li className=" "><a href="#">Coverage guidance</a></li>
                                                 </ul>
                                             </div>
                                     </Collapse>
@@ -96,29 +93,29 @@ export class MainMenu extends ReportingPageWithHeader<any> {
                         </div>
                     </div>
 
-                    <div className="col-12 col-sm-6 col-lg-4 mt-3">
+                    <div className="col-12 col-sm-6 col-lg-4 mt-3 circle">
                         <div className="card-body">
                                 <h5 className="card-title">Recently viewed</h5>
                                 <h6 className="card-subtitle mb-2 text-muted">Your last visited reports</h6>
-                            <div className="card">
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item"><a href="#">DALYs Guidance</a></li>
-                                    <li className="list-group-item"><a href="#">open call burden estimate templates</a></li>
+                            <div className="">
+                                <ul className="list-unstyled">
+                                    <li className=" "><a href="#">DALYs Guidance</a></li>
+                                    <li className=" "><a href="#">open call burden estimate templates</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
 
-                    <div className="col-12 col-sm-6 col-lg-4 mt-3">
+                    <div className="col-12 col-sm-6 col-lg-4 mt-3 rounded-circle">
                         <div className="card-body">
                             <h5 className="card-title">Popular</h5>
                             <h6 className="card-subtitle mb-2 text-muted">The most visited reports</h6>
-                            <div className="card">
+                            <div className="">
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item"><a href="#">burden estimate template tables</a></li>
-                                <li className="list-group-item"><a href="#">open call burden estimate templates</a></li>
-                                <li className="list-group-item"><a href="#">Methods for creating Kosovo demographic data</a></li>
-                                <li className="list-group-item"><a href="#">coverage data from 201708test-2</a></li>
+                                <li className=" "><a href="#">open call burden estimate templates</a></li>
+                                <li className=" "><a href="#">Methods for creating Kosovo demographic data</a></li>
+                                <li className=" "><a href="#">coverage data from 201708test-2</a></li>
                             </ul>
                             </div>
                         </div>
