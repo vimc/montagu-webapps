d44f1a705be73f31028ea587e3a2cafc1bee5ae5
Alex Hill <alex.hill@gmail.com>
2018-02-08 09:52:56 +0000

    use nginx served paths for iframe srcs and remove html folder

--- app/src/main/contrib/components/Responsibilities/Guidance/Demographics/ResponsibilityGuidanceTuvalu.tsx
+++ app/src/main/contrib/components/Responsibilities/Guidance/Demographics/ResponsibilityGuidanceTuvalu.tsx
@@ -4,7 +4,7 @@ import {ContribPageWithHeader} from "../../../PageWithHeader/ContribPageWithHead
 import { ChooseGroupPage } from "../../../ChooseGroup/ChooseGroupPage";
 import { Page } from "../../../../../shared/components/PageWithHeader/Page";
 
-const embeddedHtml = require('./html/tuvalu.html');
+const iframeSrc = "/contribution/guidance/tuvalu_demography.html";
 
 export class ResponsibilityGuidanceTuvalu extends ContribPageWithHeader<undefined> {
     name() {
@@ -25,7 +25,7 @@ export class ResponsibilityGuidanceTuvalu extends ContribPageWithHeader<undefine
 
     render() :JSX.Element {
         return <Page page={this}>
-            <iframe src={embeddedHtml} width="100%" height="13290px" frameBorder="0"></iframe>
+            <iframe src={iframeSrc} width="100%" height="13290px" frameBorder="0"></iframe>
         </Page>;
     }
 }
