befe01d84fb837bc2c87a4e5a40dc757968eaa2d
Alex Hill <alex.hill@gmail.com>
2018-02-08 13:16:18 +0000

    working prototype
    Former-commit-id: 46823856286e50410b94a78c2d7f51b050a760d2

--- app/src/main/report/components/MainMenu/MainMenu.tsx
+++ app/src/main/report/components/MainMenu/MainMenu.tsx
@@ -11,6 +11,14 @@ const testimage = require("./test.png");
 const measles = require("./measles.png");
 
 export class MainMenu extends ReportingPageWithHeader<any> {
+
+    constructor() {
+        super();
+        this.state = {
+            showResults: false
+        }
+    }
+
     load(props: undefined) {
         return this.loadParent(props).then(() => {
             return reportStore.fetchReports();
@@ -32,6 +40,24 @@ export class MainMenu extends ReportingPageWithHeader<any> {
     parent(): IPageWithParent {
         return null;
     }
+
+    showResults = (e: any) => {
+        if (e.target.value.length > 2)
+            this.setState({
+                showResults: true
+            })
+        else
+            this.setState({
+                showResults: false
+            })
+
+    };
+
+    toggleResults = (e: any) => {
+        this.setState({
+            showResults: !this.state.showResults
+        })
+    };
 
     render(): JSX.Element {
         // return <Page page={this}>
@@ -89,20 +115,9 @@ export class MainMenu extends ReportingPageWithHeader<any> {
 
                     <div className="col-12 col-lg-6">
                         <h2>Search</h2>
-                        <div className="m-2">
-                             <Typeahead minLength={2} options={["DALYs Guidance",
-                                "coverage data from 201708test-1",
-                                "coverage data from 201708test-2",
-                                "burden estimate template tables",
-                                "Create scenario-specific burden estimate templates",
-                                "open call burden estimate templates",
-                                "Discussion and methods regarding child mortality rates",
-                                "Methods for creating Kosovo demographic data",
-                                "Methods for creating Marshsall Islands demographic data"
-                            ]}/>
-                            <p>
-                                <a href="#">View all</a>
-                            </p>
+                        <div className="mt-2 mr-5">
+                            <input className="form-control form-control-lg" type="text"
+                                   onChange={this.showResults.bind(this)}/>
                         </div>
                     </div>
 
@@ -113,28 +128,51 @@ export class MainMenu extends ReportingPageWithHeader<any> {
                                 <div className="card-body">
                                     <div className={"row"}>
                                         <div className={"col-6"}>
-                                        <ul className="nav nav-pills flex-column">
-                                            <li className="list-group">
-                                                <a className="list-group-item active" href="#">Recently viewed</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="list-group-item" href="#">Popular</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="list-group-item" href="#">Example category</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="list-group-item" href="#">Another example Category</a>
-                                            </li>
-                                        </ul>
+                                            <ul className="nav nav-pills flex-column">
+                                                <li className="list-group">
+                                                    <a className="list-group-item active" href="#">Recently viewed</a>
+                                                </li>
+                                                <li className="nav-item">
+                                                    <a className="list-group-item" href="#">Popular</a>
+                                                </li>
+                                                <li className="nav-item">
+                                                    <a className="list-group-item" href="#">Example category</a>
+                                                </li>
+                                                <li className="nav-item">
+                                                    <a className="list-group-item" href="#">Another example Category</a>
+                                                </li>
+                                            </ul>
                                         </div>
                                         <div className="tab-content col-6">
-                                            <div className="tab-pane fade show active">
+                                            <div className="tab-pane fade show active"
+                                                 style={{"overflow-y": "scroll", maxHeight: "400px"}}>
                                                 <ul className="list-group list-group-flush">
-                                                    <li className="list-group-item"><a href="#">DALYs Guidance</a></li>
-                                                    <li className="list-group-item"><a href="#">Small countries
-                                                        guidance</a></li>
-                                                    <li className="list-group-item"><a href="#">Coverage guidance</a>
+                                                    <li className="list-group-item"><a href="#">A report</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">Another report</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">Something</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">A report</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">Another report</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">Something</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">A report</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">Another report</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+                                                    </li>
+                                                    <li className="list-group-item"><a href="#">Something</a>
+                                                        <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
                                                     </li>
                                                 </ul>
                                             </div>
@@ -143,6 +181,101 @@ export class MainMenu extends ReportingPageWithHeader<any> {
                                 </div>
                             </Card>
                         </div>
+                    </div>
+                </div>
+                <hr style={{marginTop: "60px", borderBottom: "0", borderStyle: "solid !important"}}/>
+                <div style={{marginTop: "-34px", paddingRight: "5px", paddingLeft: "5px", textAlign: "center"}}>
+                    <div style={{display: "inline", background: "white", zIndex: 9999, padding: "16px"}}>
+                        <button className="btn btn-success arrow-down" onClick={this.toggleResults.bind(this)}>View all
+                        </button>
+                    </div>
+                </div>
+                <div className={this.state.showResults ? "d-block" : "d-none"}>
+                    <div className="row mb-5">
+                        <div className="col-4">
+                            Sort by: <select className="form-control">
+                            <option>Date created</option>
+                            <option>Date published</option>
+                            <option>Name</option>
+                            <option>Most viewed</option>
+                        </select>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <ul className="list-unstyled ml-4">
+                            <li>
+                                <a href="/reports/guidance-201710-DALYs/20171124-110346-15208cac/">A report you're
+                                    allowed to see</a>
+                                <span className="small badge-info badge float-right ml-1">created: 28th Sep 2017</span>
+                                <span className="small badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/internal-2017-201708test-1-coverage/20170829-155605-aec5f8fb/">B
+                                report you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/internal-2017-201708test-2-coverage/20171115-100248-7ad5e28b/">C
+                                report you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small  badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/internal-2017-burden-estimates-template/20180117-165238-676f3fb0/">
+                                D report you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small   badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/201710gavi-coverage-estimates/20180205-100037-38487ecc/">Some report
+                                you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small  badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li>
+                                <a href="/reports/guidance-201710-DALYs/20171124-110346-15208cac/">A report you're
+                                    allowed to see</a>
+                                <span className="small badge-info badge float-right ml-1">created: 28th Sep 2017</span>
+                                <span className="small badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/internal-2017-201708test-1-coverage/20170829-155605-aec5f8fb/">B
+                                report you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/internal-2017-201708test-2-coverage/20171115-100248-7ad5e28b/">C
+                                report you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small  badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/internal-2017-burden-estimates-template/20180117-165238-676f3fb0/">
+                                D report you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small   badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+                            <li className="clearfix"><a
+                                href="/reports/201710gavi-coverage-estimates/20180205-100037-38487ecc/">Some report
+                                you're allowed to see</a>
+                                <span className="small ml-1 badge-info badge float-right">created: 28th Sep 2017</span>
+                                <span className="small  badge-primary badge float-right">Published: 12th Oct 2017</span>
+                                <p className="small text-muted">20180205-100037-38487ecc</p>
+                            </li>
+
+
+                        </ul>
                     </div>
                 </div>
                 <div className="row">
@@ -176,13 +309,14 @@ export class MainMenu extends ReportingPageWithHeader<any> {
                     {/*</div>*/}
                     {/*</div>*/}
 
-                    <div className="col-12 pr-0">
+                    <div className="col-12 pr-0 mt-3">
                         <h2>Featured report</h2>
-                        <p className={"muted small"}>20180205-100037-38487ecc/</p>
                         <div className="border border-dark rounded-0" style={{height: "100%"}}>
                             <div className="mb-3 mr-5 ml-5 mt-3">
                                 <h4 className="mb-3">Impact of measles campaigns 2018 - 2020 under low coverage
                                     assumptions</h4>
+                                <p className={"text-muted small"}>20180205-100037-38487ecc/</p>
+
                                 <p><a href="#">See full report</a></p>
                                 <img src={measles} className="img-fluid d-block"/>
                             </div>
