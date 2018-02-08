61dfa61d0dc109183c5bd385eb21fc552bbf8385
Alex Hill <alex.hill@gmail.com>
2018-02-08 09:52:56 +0000

    use nginx served paths for iframe srcs and remove html folder
    Former-commit-id: a1eb3a35b4f9c88bb116495187048fa8d9c73531

--- app/src/main/contrib/components/Responsibilities/Guidance/Demographics/ResponsibilityGuidanceOver80.tsx
+++ app/src/main/contrib/components/Responsibilities/Guidance/Demographics/ResponsibilityGuidanceOver80.tsx
@@ -4,7 +4,7 @@ import {ContribPageWithHeader} from "../../../PageWithHeader/ContribPageWithHead
 import { ChooseGroupPage } from "../../../ChooseGroup/ChooseGroupPage";
 import { Page } from "../../../../../shared/components/PageWithHeader/Page";
 
-const embeddedHtml = require('./html/over80.html');
+const iframeSrc = "/contribution/guidance/over80.html";
 
 export class ResponsibilityGuidanceOver80 extends ContribPageWithHeader<undefined> {
     name() {
@@ -25,7 +25,7 @@ export class ResponsibilityGuidanceOver80 extends ContribPageWithHeader<undefine
 
     render() :JSX.Element {
         return <Page page={this}>
-            <iframe src={embeddedHtml} width="100%" height="5836px" frameBorder="0"></iframe>
+            <iframe src={iframeSrc} width="100%" height="5836px" frameBorder="0"></iframe>
         </Page>;
     }
 }
