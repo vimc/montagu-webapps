22f79652c4d91d0885c4b99e2ee0d373a2b6e19d
Yuri Kalinin <>
2018-02-19 21:04:56 +0000

    report details component is connected to redux tore

--- app/src/main/report/components/Reports/ReportDetails.tsx
+++ app/src/main/report/components/Reports/ReportDetails.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
+import { Dispatch, Action } from "redux";
+import { connect } from 'react-redux';
+
 import {RemoteContent} from "../../../shared/models/RemoteContent";
 import {RemoteContentComponent} from "../../../shared/components/RemoteContentComponent/RemoteContentComponent";
-import {reportStore} from "../../stores/ReportStore";
-import {connectToStores} from "../../../shared/alt";
+
 import {Version} from "../../../shared/models/reports/Report";
 import {ParameterList} from "../Parameters/ParameterList";
 import {DataLinks} from "../Data/DataLinks";
 import {ResourceLinks} from "../Resources/ResourceLinks";
 import {ReportVersionSwitcher} from "./ReportVersionSwitcher";
 import {ArtefactsSection} from "../Artefacts/ArtefactsSection";
-import {IRouter} from "simple-react-router";
 import {DraftStamp} from "../DraftStamp";
 
 import "../../../shared/styles/common.scss";
+import {ReportAppState} from "../../reducers/reportAppReducers";
 
 interface PublicProps {
     onChangeVersion: (version: string) => void;
@@ -25,23 +27,6 @@ export interface ReportDetailsProps extends RemoteContent, PublicProps {
 }
 
 export class ReportDetailsComponent extends RemoteContentComponent<ReportDetailsProps, undefined> {
-    static getStores() {
-        return [reportStore];
-    }
-
-    static getPropsFromStores(props: Partial<ReportDetailsProps>): ReportDetailsProps {
-        const s = reportStore.getState();
-        return {
-            versionDetails: s.versionDetails[s.currentVersion],
-            report: s.currentReport,
-            allVersions: s.versions[s.currentReport],
-            ready: s.ready
-                && s.versions[s.currentReport] !== undefined
-                && s.versionDetails[s.currentVersion] != null,
-
-            onChangeVersion: props.onChangeVersion
-        };
-    }
 
     renderContent(props: ReportDetailsProps) {
         const version = props.versionDetails.id;
@@ -60,4 +45,14 @@ export class ReportDetailsComponent extends RemoteContentComponent<ReportDetails
     }
 }
 
-export const ReportDetails = connectToStores(ReportDetailsComponent) as ComponentConstructor<PublicProps, undefined>;
+export const mapStateToProps = (state: ReportAppState, props: any): Partial<ReportDetailsProps> => {
+    return {
+        versionDetails: state.reports.versionDetails,
+        ready: !!state.reports.versionDetails,
+        report: state.reports.currentReport,
+        allVersions: state.reports.versions,
+        onChangeVersion: props.onChangeVersion
+    }
+};
+
+export const ReportDetails = connect(mapStateToProps)(ReportDetailsComponent);
