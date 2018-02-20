3ff1a99469850229e5c12a7a922b3b2da55fb272
Yuri Kalinin <>
2018-02-20 10:20:11 +0000

    fixed tests

--- app/src/test/report/components/Reports/ReportPageTests.tsx
+++ app/src/test/report/components/Reports/ReportPageTests.tsx
@@ -42,48 +42,21 @@ describe("ReportPage", () => {
         });
     });
 
-    it("triggers actions on mount", (done: DoneCallback) => {
-        const store = createMockStore({ auth: {loggedIn: true, username: "test.user"} });
-
-        const location = mockLocation<ReportPageProps>({report: "report", version: "v1"});
-        sandbox.setStubFunc(ReportPageComponent.prototype, "getLocationParams", () => (location));
-        sandbox.setStubFunc(ReportPageComponent.prototype, "render", () => (<p/>));
-        const createBreadCrumbSpy = sandbox.setStub(ReportPageComponent.prototype, "createBreadcrumb");
-        sandbox.setStub(ReportsService.prototype, "getReportVersions");
-        sandbox.setStub(ReportsService.prototype, "getVersionDetails");
-
-        const rendered = mount(<Provider store={store}><ReportPage /></Provider>);
-        setTimeout(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).to.eql(ReportTypeKeys.SET_CURRENT_REPORT);
-            expect(actions[1].type).to.eql(ReportTypeKeys.SET_CURRENT_VERSION);
-            expect(actions[2].type).to.eql(ReportTypeKeys.REPORT_VERSIONS_FETCHED);
-            expect(actions[3].type).to.eql(ReportTypeKeys.REPORT_VERSION_DETAILS_FETCHED);
-            expect(createBreadCrumbSpy.called).to.eq(true);
-            done();
-        });
-    });
-
-    it("triggers actions and redirect when child triggers changeVersion", () => {
+    it("triggers actions and redirect when child triggers changeVersion", (done: DoneCallback) => {
         const redirectTo = sandbox.sinon.stub();
         const router: IRouter = {redirectTo};
         sandbox.setStub(ReportPageComponent.prototype, "loadVersion");
         const location = mockLocation<ReportPageProps>({report: "reportname", version: "oldVersion"});
         const page = new ReportPageComponent({location: location, router: router});
-        const promise = page.changeVersion("versionname")
-        expect(redirectTo.called).to.equal(true, "Expected redirectTo to be called");
-        expect(redirectTo.calledWith("/reportname/versionname/"));
-        return promise;
+        page.changeVersion("versionname")
+        setTimeout(() => {
+            expect(redirectTo.called).to.equal(true, "Expected redirectTo to be called");
+            expect(redirectTo.calledWith("/reportname/versionname/"));
+            done();
+        });
     });
 
     const location = mockLocation<ReportPageProps>({report: "report", version: "v1"});
     const page = new ReportPageComponent({location: location, router: null});
-    addNavigationTests(page, sandbox, () => {
-        const version = mockVersion({name: "report", id: "v1"});
-        mockFetcherForMultipleResponses([
-            mockReportsEndpoint([mockReport({name: "report"})]),
-            mockReportVersionsEndpoint("report", [version, mockVersion()]),
-            mockReportDetailsEndpoint(version)
-        ])
-    });
+    addNavigationTests(page, sandbox, () => {});
 });
