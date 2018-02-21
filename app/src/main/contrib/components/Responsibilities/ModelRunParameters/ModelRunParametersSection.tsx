823abf3d5581e242b35a3050659824e1219f313b
Alex Hill <alex.hill@gmail.com>
2018-02-21 18:09:37 +0000

    dashed hr

--- app/src/main/contrib/components/Responsibilities/ModelRunParameters/ModelRunParametersSection.tsx
+++ app/src/main/contrib/components/Responsibilities/ModelRunParameters/ModelRunParametersSection.tsx
@@ -13,7 +13,7 @@ export class ModelRunParametersSection extends React.Component<Props, undefined>
 
         return <div>
             <h2 className="largeSectionTitle mb-0">Disease: {this.props.disease}</h2>
-            <hr className="mt-1"/>
+            <hr className="mt-1 dashed"/>
             <ModelRunParametersStatus disease={this.props.disease} />
             <ModelRunParametersForm disease={this.props.disease} url={this.props.url} />
         </div>
