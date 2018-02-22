4fd2526c4dead4643ece9a65882f0ea2d533af02
Alex Hill <alex.hill@gmail.com>
2018-02-22 11:12:04 +0000

    make logo 75px high

--- app/src/main/shared/components/PageWithHeader/PageHeader.tsx
+++ app/src/main/shared/components/PageWithHeader/PageHeader.tsx
@@ -13,7 +13,7 @@ export class PageHeader extends React.Component<HeaderProps, undefined> {
     render() {
         return <div>
             <header className="header">
-                <a href="/"><img src={this.props.logo} className="logo" height="80" alt="VIMC"/></a>
+                <a href="/"><img src={this.props.logo} className="logo" height="75" alt="VIMC"/></a>
                 <div className="header__siteTitle">
                     <InternalLink href="/">{this.props.siteTitle}</InternalLink>
                 </div>
