396ed2bf9b2dda40e57731dc62e20ce105da4139
Yuri Kalinin <>
2018-02-25 02:37:08 +0000

    report list item as pure function

--- app/src/main/report/components/ReportsList/ReportListItem.tsx
+++ app/src/main/report/components/ReportsList/ReportListItem.tsx
@@ -2,14 +2,12 @@ import * as React from "react";
 import {InternalLink} from "../../../shared/components/InternalLink";
 import {Report} from "../../../shared/models/Generated";
 
-export class ReportListItem extends React.Component<Report, undefined> {
-    render() {
-        const url = `/${this.props.name}/${this.props.latest_version}/`;
-        let name = this.props.name;
-        if (this.props.display_name) {
-            name = this.props.display_name;
-        }
-
-        return <li><InternalLink href={url}>{name}</InternalLink></li>;
-    }
+export const ReportListItem: React.StatelessComponent<Report> = (props: Report) => {
+    const url = `/${props.name}/${props.latest_version}/`;
+    let name = props.display_name ? props.display_name : props.name;
+    return (
+        <li>
+            <InternalLink href={url}>{name}</InternalLink>
+        </li>
+    );
 }
