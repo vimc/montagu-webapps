1f342545187b2f2a323231997e67de038e12263a
Yuri Kalinin <>
2018-02-25 01:51:09 +0000

    tests fixed

--- app/src/test/report/components/ReportsList/ReportListContainerTests.tsx
+++ app/src/test/report/components/ReportsList/ReportListContainerTests.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import { expect } from "chai";
-import { shallow } from "enzyme";
 import { mockReport } from "../../../mocks/mockModels";
-import {ReportsListComponent} from "../../../../main/report/components/ReportsList/ReportsListComponent";
-import {mapStateToProps} from "../../../../main/report/components/ReportsList/ReportsList";
-import {ReportListItem} from "../../../../main/report/components/ReportsList/ReportListItem";
+import {getDisplayedReportsList, mapStateToProps} from "../../../../main/report/components/ReportsList/ReportsList";
 import {mockReportAppState} from "../../../mocks/mockStates";
+import {ReportsSortingFields} from "../../../../main/report/actionTypes/ReportsActionsTypes";
 
 describe("ReportListComponent", () => {
 
@@ -21,4 +19,26 @@ describe("ReportListComponent", () => {
         expect(props.reports.length).to.eql(2);
         expect(props.ready).to.eql(true);
     });
+
+    it('sorts reports list by name', () => {
+        const reportsMock = [mockReport({name: "b"}), mockReport({name: "c"}), mockReport({name: "a"})];
+        const sortedReports = getDisplayedReportsList(reportsMock, ReportsSortingFields.name);
+        expect(sortedReports[0].name).to.eql("a");
+        expect(sortedReports[1].name).to.eql("b");
+        expect(sortedReports[2].name).to.eql("c");
+    });
+
+    it('sorts reports list by latest version', () => {
+        const reportsMock = [
+            mockReport({name: "b", latest_version: "20170327-002851-dd944766"}),
+            mockReport({name: "c", latest_version: "20170326-002851-dd944766"}),
+            mockReport({name: "a", latest_version: "20170328-002851-dd944766"})
+        ];
+        const sortedReports = getDisplayedReportsList(reportsMock, ReportsSortingFields.latest_version);
+        expect(sortedReports[0].name).to.eql("c");
+        expect(sortedReports[1].name).to.eql("b");
+        expect(sortedReports[2].name).to.eql("a");
+    });
+
 });
+
--- app/src/test/report/components/ReportsList/ReportsListSortingTests.tsx
+++ app/src/test/report/components/ReportsList/ReportsListSortingTests.tsx
@@ -5,24 +5,25 @@ import { shallow } from "enzyme";
 import "../../../helper";
 import {ReportsListSortingComponent} from "../../../../main/report/components/ReportsList/ReportsListSorting";
 import {Sandbox} from "../../../Sandbox";
+import {ReportsSortingFields} from "../../../../main/report/actionTypes/ReportsActionsTypes";
 
 describe("ReportListSorting", () => {
 
     const sandbox = new Sandbox();
     afterEach(() => sandbox.restore());
 
-    // it("can render select with 3 elements", () => {
-    //     const rendered = shallow(<ReportsListSortingComponent sort={()=>{}} />);
-    //     const items = rendered.find('option');
-    //     expect(items).to.have.length(2);
-    // });
-    //
-    // it("triggers sort on select change", () => {
-    //     const sortSpy = sandbox.createSpy();
-    //     const rendered = shallow(<ReportsListSortingComponent sort={sortSpy} />);
-    //     const items = rendered.find('select').simulate('change', {target: { value : 'name'}});
-    //     expect(sortSpy.called).is.equal(true);
-    //     expect(sortSpy.getCall(0).args).is.eql(["name"]);
-    // });
+    it("can render select with 3 elements", () => {
+        const rendered = shallow(<ReportsListSortingComponent sort={()=>{}} sortBy={ReportsSortingFields.name} />);
+        const items = rendered.find('option');
+        expect(items).to.have.length(2);
+    });
+
+    it("triggers sort on select change", () => {
+        const sortSpy = sandbox.createSpy();
+        const rendered = shallow(<ReportsListSortingComponent sort={sortSpy} sortBy={ReportsSortingFields.name} />);
+        const items = rendered.find('select').simulate('change', {target: { value : 'name'}});
+        expect(sortSpy.called).is.equal(true);
+        expect(sortSpy.getCall(0).args).is.eql(["name"]);
+    });
 
 });
