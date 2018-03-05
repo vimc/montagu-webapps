e50b65edd71425a9b5a420cb252a01f399f45637
Alex Hill <alex.hill@gmail.com>
2018-03-05 17:07:56 +0000

    calendar

--- app/src/main/report/components/ReportsList/ReportsListFilter.tsx
+++ app/src/main/report/components/ReportsList/ReportsListFilter.tsx
@@ -7,6 +7,7 @@ import {reportsActions} from "../../actions/reportsActions";
 import {ReportsFilterFields, ReportsFilterPublishTypes,} from "../../actionTypes/ReportsActionsTypes";
 import {ReportAppState} from "../../reducers/reportAppReducers";
 import {DatePicker} from "../../../shared/components/DatePicker/DatePicker";
+import {CalendarIcon} from "./Calendar";
 
 interface ReportsListFilterProps {
     filterData: ReportsFilterFields;
@@ -65,12 +66,8 @@ export const ReportsListFilterComponent: React.StatelessComponent<ReportsListFil
                         <label className={"mb-0 small font-weight-bold"}>From</label>
                         <div className="input-group mb-3">
                             <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1">
-                                 <svg viewBox="0 0 14 16" width="14" height="16"
-                                      aria-hidden="true">
-                        <path fillRule="evenodd"
-                              d="M13 2h-1v1.5c0 .28-.22.5-.5.5h-2c-.28 0-.5-.22-.5-.5V2H6v1.5c0 .28-.22.5-.5.5h-2c-.28 0-.5-.22-.5-.5V2H2c-.55 0-1 .45-1 1v11c0 .55.45 1 1 1h11c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm0 12H2V5h11v9zM5 3H4V1h1v2zm6 0h-1V1h1v2zM6 7H5V6h1v1zm2 0H7V6h1v1zm2 0H9V6h1v1zm2 0h-1V6h1v1zM4 9H3V8h1v1zm2 0H5V8h1v1zm2 0H7V8h1v1zm2 0H9V8h1v1zm2 0h-1V8h1v1zm-8 2H3v-1h1v1zm2 0H5v-1h1v1zm2 0H7v-1h1v1zm2 0H9v-1h1v1zm2 0h-1v-1h1v1zm-8 2H3v-1h1v1zm2 0H5v-1h1v1zm2 0H7v-1h1v1zm2 0H9v-1h1v1z">
-                        </path></svg>
+                            <span className="input-group-text">
+                                <CalendarIcon/>
                             </span>
                             </div>
                             <DatePicker
@@ -85,12 +82,8 @@ export const ReportsListFilterComponent: React.StatelessComponent<ReportsListFil
                         <label className={"mb-0 small font-weight-bold"}> Until</label>
                         <div className="input-group mb-3 picker-on-right">
                             <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1">
-                                 <svg viewBox="0 0 14 16" width="14" height="16"
-                                      aria-hidden="true">
-                        <path fillRule="evenodd"
-                              d="M13 2h-1v1.5c0 .28-.22.5-.5.5h-2c-.28 0-.5-.22-.5-.5V2H6v1.5c0 .28-.22.5-.5.5h-2c-.28 0-.5-.22-.5-.5V2H2c-.55 0-1 .45-1 1v11c0 .55.45 1 1 1h11c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm0 12H2V5h11v9zM5 3H4V1h1v2zm6 0h-1V1h1v2zM6 7H5V6h1v1zm2 0H7V6h1v1zm2 0H9V6h1v1zm2 0h-1V6h1v1zM4 9H3V8h1v1zm2 0H5V8h1v1zm2 0H7V8h1v1zm2 0H9V8h1v1zm2 0h-1V8h1v1zm-8 2H3v-1h1v1zm2 0H5v-1h1v1zm2 0H7v-1h1v1zm2 0H9v-1h1v1zm2 0h-1v-1h1v1zm-8 2H3v-1h1v1zm2 0H5v-1h1v1zm2 0H7v-1h1v1zm2 0H9v-1h1v1z">
-                        </path></svg>
+                            <span className="input-group-text">
+                                <CalendarIcon/>
                             </span>
                             </div>
                             <DatePicker
