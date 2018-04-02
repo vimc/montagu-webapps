5adc7cdcfdbe8e027930639cafbf433631228806
Yuri Kalinin <>
2018-04-02 06:54:49 +0100

    beinning of download demogrqphics page

---
+++ app/src/main/contrib/actions/pages/downloadDemographicsPageActionCreators.ts
@@ -0,0 +1,31 @@
+import { Dispatch, Action } from "redux";
+
+import { modellingGroupsActionCreators } from "../modellingGroupsActionCreators";
+import {breadcrumbsActions} from "../../../shared/actions/breadcrumbsActions";
+import {touchstonesActionCreators} from "../touchstonesActionCreators";
+import {ContribAppState} from "../../reducers/contribAppReducers";
+import {
+    ResponsibilityOverviewPageComponent,
+    ResponsibilityOverviewPageLocationProps,
+} from "../../components/Responsibilities/Overview/ResponsibilityOverviewPage";
+import {responsibilitiesActionCreators} from "../responsibilitiesActionCreators";
+import {diseasesActionCreators} from "../diseasesActionCreators";
+
+export const responsibilityOverviewPageActionCreators = {
+
+    onLoad(props: ResponsibilityOverviewPageLocationProps) {
+        return async (dispatch: Dispatch<any>, getState: () => any) => {
+            await dispatch(modellingGroupsActionCreators.getUserGroups());
+            await dispatch(diseasesActionCreators.getAllDiseases());
+            dispatch(modellingGroupsActionCreators.setCurrentGroup(props.groupId));
+            await dispatch(touchstonesActionCreators.getTouchstonesByGroupId(props.groupId));
+            dispatch(touchstonesActionCreators.setCurrentTouchstone(props.touchstoneId));
+            dispatch(breadcrumbsActions.createBreadcrumbs(ResponsibilityOverviewPageComponent.breadcrumb(getState())));
+            dispatch(responsibilitiesActionCreators.getResponsibilitySet(
+                getState().groups.currentUserGroup,
+                getState().touchstones.currentTouchstone
+            ));
+        }
+    }
+
+};
--- app/src/main/contrib/components/Responsibilities/Demographics/DownloadDemographicsPage.tsx
+++ app/src/main/contrib/components/Responsibilities/Demographics/DownloadDemographicsPage.tsx
@@ -1,47 +1,83 @@
 import * as React from "react";
+import { Action, Dispatch } from "redux";
+import { compose} from "recompose";
+import { connect } from 'react-redux';
+
 import { DownloadDataTitle } from "../DownloadDataTitle";
 import { touchstoneActions } from "../../../actions/TouchstoneActions";
 import { demographicStore } from "../../../stores/DemographicStore";
 import { DownloadDemographicsContent } from "./DownloadDemographicsContent";
 import {ContribPageWithHeader} from "../../PageWithHeader/ContribPageWithHeader";
 import {IPageWithParent} from "../../../../shared/models/Breadcrumb";
-import {ResponsibilityOverviewPage} from "../Overview/ResponsibilityOverviewPage";
+import {
+    ResponsibilityOverviewPage, ResponsibilityOverviewPageComponent, ResponsibilityOverviewPageLocationProps,
+    ResponsibilityOverviewPageProps
+} from "../Overview/ResponsibilityOverviewPage";
 import { Page } from "../../../../shared/components/PageWithHeader/Page";
-import {ChooseActionPage} from "../../ChooseAction/ChooseActionPage";
+import {ChooseActionPage, ChooseActionPageComponent} from "../../ChooseAction/ChooseActionPage";
+import {PageBreadcrumb, PageProperties} from "../../../../shared/components/PageWithHeader/PageWithHeader";
+import {ContribAppState} from "../../../reducers/contribAppReducers";
+import {PageArticle} from "../../../../shared/components/PageWithHeader/PageArticle";
+import {responsibilityOverviewPageActionCreators} from "../../../actions/pages/_ResponsibilityOverviewPageActionCreators";
+import {ModellingGroup, Touchstone} from "../../../../shared/models/Generated";
+import {IExtendedResponsibilitySet} from "../../../models/ResponsibilitySet";
 
-interface LocationProps {
+interface DownloadDemographicsPageLocationProps {
     groupId: string;
     touchstoneId: string;
 }
 
-export class DownloadDemographicsPage extends ContribPageWithHeader<LocationProps> {
-    load(props: LocationProps) {
-        return new ChooseActionPage().load(props).then(() => {
-            touchstoneActions.setCurrentTouchstone(props.touchstoneId);
-            return demographicStore.fetchDataSets();
-        });
+export interface DownloadDemographicsPageProps extends PageProperties<DownloadDemographicsPageLocationProps> {
+    touchstone: Touchstone;
+    group: ModellingGroup;
+}
+
+export class DownloadDemographicsPageComponent extends React.Component<DownloadDemographicsPageProps> {
+    componentDidMount() {
+        this.props.onLoad(this.props.match.params)
     }
 
-    name() {
-        return "Download demographic data sets";
+    static breadcrumb(state: ContribAppState): PageBreadcrumb {
+        return {
+            name: "Download demographic data sets",
+            urlFragment: "demographics/",
+            parent: ResponsibilityOverviewPageComponent.breadcrumb(state)
+        }
     }
+
+    // load(props: LocationProps) {
+    //     return new ChooseActionPage().load(props).then(() => {
+    //         touchstoneActions.setCurrentTouchstone(props.touchstoneId);
+    //         return demographicStore.fetchDataSets();
+    //     });
+    // }
 
     title(): JSX.Element {
         return <DownloadDataTitle title="Download demographic data sets" />
     }
 
-    urlFragment(): string {
-        return "demographics/";
-    }
-
-    parent(): IPageWithParent {
-        return new ResponsibilityOverviewPage();
-    }
-
     render(): JSX.Element {
-        return <Page page={this}>
+        return <PageArticle title={this.title()}>
             <DownloadDemographicsContent />
-        </Page>;
+        </PageArticle>;
     }
 
 }
+
+export const mapStateToProps = (state: ContribAppState): Partial<DownloadDemographicsPageProps> => {
+    console.log(2224, state)
+    return {
+        touchstone: state.touchstones.currentTouchstone,
+        group: state.groups.currentUserGroup
+    }
+};
+
+export const mapDispatchToProps = (dispatch: Dispatch<Action>): Partial<DownloadDemographicsPageProps> => {
+    return {
+        onLoad: (params: DownloadDemographicsPageLocationProps) => dispatch(responsibilityOverviewPageActionCreators.onLoad(params))
+    }
+};
+
+export const DownloadDemographicsPage = compose(
+    connect(mapStateToProps, mapDispatchToProps),
+)(DownloadDemographicsPageComponent) as React.ComponentClass<Partial<DownloadDemographicsPageProps>>;
