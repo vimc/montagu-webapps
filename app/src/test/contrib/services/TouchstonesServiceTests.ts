4b3470056d09afda8c2cf9c51c7bae193deb03b3
Yuri Kalinin <>
2018-04-14 05:27:04 +0100

    touchstone service

---
+++ app/src/test/contrib/services/TouchstonesServiceTests.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { createMemoryHistory } from 'history';
+
+import {createContribStore} from "../../../main/contrib/createStore";
+import {TouchstonesService} from "../../../main/contrib/services/TouchstonesService";
+import {Sandbox} from "../../Sandbox";
+import {ContribAppState} from "../../../main/contrib/reducers/contribAppReducers";
+
+describe('Touchstones service tests', () => {
+    const sandbox = new Sandbox();
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('fetches touchstones', () => {
+        const history = createMemoryHistory({initialEntries: [ '/asd' ]});
+        const store = createContribStore(history)
+        const touchstoneService = new TouchstonesService(store.dispatch, store.getState as () => ContribAppState);
+
+        const setOptionsSpy = sandbox.setSpy(touchstoneService, "setOptions");
+
+        const getStub = sandbox.setStubFunc(touchstoneService, "get", ()=>{
+            return Promise.resolve();
+        });
+
+        touchstoneService.getTouchstonesByGroupId('group-1');
+
+        expect(getStub.getCall(0).args[0]).to.equal('/modelling-groups/group-1/responsibilities/');
+        expect(setOptionsSpy.getCall(0).args[0]).to.eql({ cacheKey: 'touchstones' });
+    })
+
+
+})
