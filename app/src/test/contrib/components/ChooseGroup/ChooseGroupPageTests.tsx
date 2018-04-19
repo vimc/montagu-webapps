32ece0cf2e93954527bbbd08efbfbf868861cbc8
Yuri Kalinin <>
2018-04-19 06:22:13 +0100

    Choose group Page component test

---
+++ app/src/test/contrib/components/ChooseGroup/ChooseGroupPageTests.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import { expect } from "chai";
+
+import "../../../helper";
+import { ChooseGroupPage, ChooseGroupPageComponent } from "../../../../main/contrib/components/ChooseGroup/ChooseGroupPage";
+import { Sandbox } from "../../../Sandbox";
+import {createMockStore} from "../../../mocks/mockStore";
+import {chooseGroupPageActionCreators} from "../../../../main/contrib/actions/pages/chooseGroupPageActionCreators";
+import {PageArticle} from "../../../../main/shared/components/PageWithHeader/PageArticle";
+import {ChooseGroupContent} from "../../../../main/contrib/components/ChooseGroup/ChooseGroupContent";
+
+describe("Choose Group Page", () => {
+
+    const sandbox = new Sandbox();
+    afterEach(() => sandbox.restore());
+
+    it("renders component with mapped props on top level", () => {
+        let store = createMockStore();
+        const rendered = shallow(<ChooseGroupPage/>, {context: {store: store}});
+        expect(typeof rendered.props().onLoad).is.equal('function');
+    });
+
+    it("renders component and mounts on 2nd level", () => {
+        let store = createMockStore();
+        const onLoadStub = sandbox.setStubFunc(chooseGroupPageActionCreators, "onLoad", () => ({type: 'any'}));
+        const rendered = shallow(<ChooseGroupPage/>, {context: {store: store}}).dive();
+        const pageArticle = rendered.find('PageArticle');
+        expect(onLoadStub.called).is.equal(true);
+        expect(pageArticle.props().title).is.equal(ChooseGroupPageComponent.title);
+        expect(pageArticle.find('p').length).is.equal(1);
+        expect(pageArticle.find(ChooseGroupContent).length).is.equal(1);
+    });
+
+
+});
+
