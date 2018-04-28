84cf18906d76a2204f7e4cf4751066fa4b08344e
Yuri Kalinin <>
2018-04-28 22:30:26 +0100

    report publish switcher tests fix

--- app/src/test/report/components/Reports/PublishSwitchTests.tsx
+++ app/src/test/report/components/Reports/PublishSwitchTests.tsx
@@ -18,7 +18,7 @@ import {ContribAppState} from "../../../../main/contrib/reducers/contribAppReduc
 import {createMockStore} from "../../../mocks/mockStore";
 
 
-describe("Publish Switch components tests", () => {
+describe("Publish Switch connected component tests", () => {
 
     const testingProps = {
         name: "report-name",
@@ -36,33 +36,45 @@ describe("Publish Switch components tests", () => {
         sandbox.restore();
     });
 
-    // it("renders on connect level", () => {
-    //     const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}});
-    //     expect(rendered.props().name).to.eql(testingProps.name);
-    //     expect(rendered.props().version).to.eql(testingProps.version);
-    //     expect(rendered.props().published).to.eql(testingProps.published);
-    //     expect(typeof rendered.props().publish).to.eql("function");
-    //     expect(typeof rendered.props().unpublish).to.eql("function");
-    // });
-    //
-    // it("renders component, report is published", () => {
-    //     const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}}).dive();
-    //     expect(rendered.find(Toggle).prop("active")).to.be.true;
-    // });
+    it("renders on connect level", () => {
+        const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}});
+        expect(rendered.props().name).to.eql(testingProps.name);
+        expect(rendered.props().version).to.eql(testingProps.version);
+        expect(rendered.props().published).to.eql(testingProps.published);
+        expect(typeof rendered.props().publish).to.eql("function");
+        expect(typeof rendered.props().unpublish).to.eql("function");
+    });
 
-    // it("renders component, toggle is on if report is published", () => {
-    //     const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}}).dive();
-    //     expect(rendered.find(Toggle).prop("active")).to.be.true;
-    // });
+    it("test dispatch publish", () => {
+        const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}}).dive();
+        const PublishSwitchComponentInstance = rendered.instance() as PublishSwitchComponent;
+        const publishStub = sandbox.setStubReduxAction(reportActionCreators, "publishReport");
+        PublishSwitchComponentInstance.props.publish("report", "v1");
+        expect(publishStub.called).to.be.true;
+        expect(publishStub.getCall(0).args[0]).to.equal("report");
+        expect(publishStub.getCall(0).args[1]).to.equal("v1");
+    });
 
-    // it("renders component, toggle is off if report is not published", () => {
-    //     testingProps
-    //     const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}}).dive();
-    //     expect(rendered.find(Toggle).prop("active")).to.be.true;
-    // });
+    it("test dispatch unpublish", () => {
+        const rendered = shallow(<PublishSwitch {...testingProps}/>, {context: {store}}).dive();
+        const PublishSwitchComponentInstance = rendered.instance() as PublishSwitchComponent;
+        const unPublishStub = sandbox.setStubReduxAction(reportActionCreators, "unPublishReport");
+        PublishSwitchComponentInstance.props.unpublish("report", "v1");
+        expect(unPublishStub.called).to.be.true;
+        expect(unPublishStub.getCall(0).args[0]).to.equal("report");
+        expect(unPublishStub.getCall(0).args[1]).to.equal("v1");
+    });
+
+});
+
+describe("Publish Switch component tests", () => {
+
+    const sandbox = new Sandbox();
+    afterEach(() => {
+        sandbox.restore();
+    });
 
     it("is off if report is not published", () => {
-
         const props: Props = {
             name: "report-name",
             version: "v1",
@@ -74,69 +86,43 @@ describe("Publish Switch components tests", () => {
         const rendered = shallow(<PublishSwitchComponent {...props} />);
         expect(rendered.find(Toggle).prop("active")).to.be.false
     });
-    //
-    // it("unpublishes on toggle if report is published", () => {
-    //
-    //     const unpublishSpy = sandbox.createSpy();
-    //     const props: Props = {
-    //         name: "report-name",
-    //         version: "v1",
-    //         published: true,
-    //         publish: sandbox.createSpy(),
-    //         unpublish: unpublishSpy
-    //     };
-    //
-    //     const rendered = shallow(<PublishSwitchComponent {...props} />);
-    //     const toggle = rendered.find(Toggle);
-    //     toggle.simulate("click");
-    //
-    //     expect(unpublishSpy.calledWith("report-name", "v1")).to.be.true;
-    //
-    // });
-    //
-    // it("publishes on toggle if report is not published", () => {
-    //
-    //     const publishSpy = sandbox.createSpy();
-    //     const props: Props = {
-    //         name: "report-name",
-    //         version: "v1",
-    //         published: false,
-    //         publish: publishSpy,
-    //         unpublish: sandbox.createSpy()
-    //     };
-    //
-    //     const rendered = shallow(<PublishSwitchComponent {...props} />);
-    //     const toggle = rendered.find(Toggle);
-    //     toggle.simulate("click");
-    //
-    //     expect(publishSpy.calledWith("report-name", "v1")).to.be.true;
-    //
-    // });
-    //
-    // it("maps publish action to props", () => {
-    //
-    //     const dispatchSpy = sandbox.createSpy();
-    //     const props = mapDispatchToProps(dispatchSpy);
-    //
-    //     const reportActionsPublishSpy = sandbox.setSpy(reportActionCreators, "publishReport");
-    //
-    //     props.publish("report", "v1");
-    //     expect(dispatchSpy.called).to.be.true;
-    //     expect(reportActionsPublishSpy.calledWith("report", "v1")).to.be.true;
-    //
-    // });
-    //
-    // it("maps unpublish action to props", () => {
-    //
-    //     const dispatchSpy = sandbox.createSpy();
-    //     const props = mapDispatchToProps(dispatchSpy);
-    //
-    //     const reportActionsUnpublishSpy = sandbox.setSpy(reportActionCreators, "unPublishReport");
-    //
-    //     props.unpublish("report", "v1");
-    //     expect(dispatchSpy.called).to.be.true;
-    //     expect(reportActionsUnpublishSpy.calledWith("report", "v1")).to.be.true;
-    //
-    // });
+
+    it("unpublishes on toggle if report is published", () => {
+
+        const unpublishSpy = sandbox.createSpy();
+        const props: Props = {
+            name: "report-name",
+            version: "v1",
+            published: true,
+            publish: sandbox.createSpy(),
+            unpublish: unpublishSpy
+        };
+
+        const rendered = shallow(<PublishSwitchComponent {...props} />);
+        const toggle = rendered.find(Toggle);
+        toggle.simulate("click");
+
+        expect(unpublishSpy.calledWith("report-name", "v1")).to.be.true;
+
+    });
+
+    it("publishes on toggle if report is not published", () => {
+
+        const publishSpy = sandbox.createSpy();
+        const props: Props = {
+            name: "report-name",
+            version: "v1",
+            published: false,
+            publish: publishSpy,
+            unpublish: sandbox.createSpy()
+        };
+
+        const rendered = shallow(<PublishSwitchComponent {...props} />);
+        const toggle = rendered.find(Toggle);
+        toggle.simulate("click");
+
+        expect(publishSpy.calledWith("report-name", "v1")).to.be.true;
+
+    });
 
 });
