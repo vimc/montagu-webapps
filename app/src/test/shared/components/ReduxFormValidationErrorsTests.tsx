d52235079309b5b4d1a0715043c5bf70e7978090
Alex Hill <alex.hill@gmail.com>
2018-06-11 11:25:58 +0100

    update test

--- app/src/test/shared/components/ReduxFormValidationErrorsTests.tsx
+++ app/src/test/shared/components/ReduxFormValidationErrorsTests.tsx
@@ -9,14 +9,14 @@ describe("ReduxFormValidationErrors", () => {
     it("should return null if no errors", () => {
 
         const rendered = shallow(<ReduxFormValidationErrors errors={[]}/>);
-        expect(rendered.find(Alert)).to.have.lengthOf(0);
+        expect(rendered.find("div")).to.have.lengthOf(0);
     });
 
     it("should return all error messages", () => {
 
         const rendered = shallow(<ReduxFormValidationErrors errors={[{message: "first error", code: "e"},
             {code: "e", message: "second error"}]}/>);
-        expect(rendered.find(Alert).childAt(0).text()).to.eq("first error, second error")
+        expect(rendered.find("div").childAt(0).text()).to.eq("first error, second error")
     })
 
 });
