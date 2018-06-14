642664ef84941eb9dcd90b4131b3e1d66e11670b
Martin Eden <martin.s.eden@pm.me>
2018-06-14 12:16:11 +0100

    Remove link for now

--- app/src/main/admin/components/Touchstones/List/TouchstoneListItem.tsx
+++ app/src/main/admin/components/Touchstones/List/TouchstoneListItem.tsx
@@ -4,14 +4,11 @@ import {Touchstone, User} from "../../../../shared/models/Generated";
 
 export class TouchstoneListItem extends React.Component<Touchstone, undefined> {
     render() {
-        const url = `/touchstones/${ this.props.id }/`;
+        // The API is guaranteed to return version in descending order
         const latestVersion = this.props.versions[0];
-
         return <tr>
             <td>{this.props.id}</td>
-            <td>
-                <InternalLink href={url}>{this.props.description}</InternalLink>
-            </td>
+            <td>{this.props.description}</td>
             <td>{this.props.comment}</td>
             <td>{latestVersion.id}</td>
         </tr>;
