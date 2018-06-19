03cb8aeee2fa355986ced48b0eb7007627c6acb5
Alex Hill <alex.hill@gmail.com>
2018-06-19 12:28:32 +0100

    include header

--- app/src/main/admin/components/AdminNoRouteFoundPage.tsx
+++ app/src/main/admin/components/AdminNoRouteFoundPage.tsx
@@ -1,18 +1,19 @@
 import * as React from "react";
-import { compose } from "recompose";
+import {compose} from "recompose";
 
-import { NoRouteFound } from "../../shared/components/NoRouteFound";
+import {NoRouteFound} from "../../shared/components/NoRouteFound";
 import {MainMenuNew} from "./MainMenu/MainMenuNew";
 import {PageBreadcrumb, PageProperties} from "../../shared/components/PageWithHeader/PageWithHeader";
 import {BreadcrumbInitializer} from "../../shared/components/Breadcrumbs/BreadcrumbsInitializer";
 import {PageArticle} from "../../shared/components/PageWithHeader/PageArticle";
+import {AdminPageHeader} from "./AdminPageHeader";
 
 export class AdminNoRouteFoundPageComponent extends React.Component<PageProperties<undefined>> {
-    componentDidMount(){
+    componentDidMount() {
         this.props.createBreadcrumbs(MainMenuNew.breadcrumb());
     }
 
-    static breadcrumb() : PageBreadcrumb {
+    static breadcrumb(): PageBreadcrumb {
         return {
             name: NoRouteFound.title(),
             urlFragment: null,
@@ -21,9 +22,12 @@ export class AdminNoRouteFoundPageComponent extends React.Component<PageProperti
     }
 
     render(): JSX.Element {
-        return <PageArticle title={NoRouteFound.title()}>
-            {NoRouteFound.render()}
-        </PageArticle>;
+        return <div>
+            <AdminPageHeader/>
+            <PageArticle title={NoRouteFound.title()}>
+                {NoRouteFound.render()}
+            </PageArticle>
+        </div>;
     }
 }
 
