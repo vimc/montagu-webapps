30d07ad61fdf4b8f673211c89124157d9e5d9fb6
Alex Hill <alex.hill@gmail.com>
2018-07-23 12:31:58 +0100

    update model generator

--- app/src/webmodels/generate/src/main/kotlin/org/vaccineimpact/api/GenerateTypeDefinitions.kt
+++ app/src/webmodels/generate/src/main/kotlin/org/vaccineimpact/api/GenerateTypeDefinitions.kt
@@ -4,27 +4,24 @@ import me.ntrrgc.tsGenerator.ClassTransformer
 import me.ntrrgc.tsGenerator.TypeScriptGenerator
 import me.ntrrgc.tsGenerator.camelCaseToSnakeCase
 import org.vaccineimpact.api.models.*
-import org.vaccineimpact.api.models.responsibilities.*
-import org.vaccineimpact.api.models.permissions.*
+import org.vaccineimpact.api.models.permissions.AssociateRole
+import org.vaccineimpact.api.models.responsibilities.ResponsibilitySet
+import org.vaccineimpact.api.models.responsibilities.ResponsibilitySetWithExpectations
 import java.io.File
+import java.sql.Timestamp
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.sql.Timestamp
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>)
-{
-    if (args.size != 1)
-    {
+fun main(args: Array<String>) {
+    if (args.size != 1) {
         println("Please specify the output path as the single command line parameter.")
         println("Output path should be the path to a .ts file.")
         exitProcess(-1)
-    }
-    else
-    {
+    } else {
         val outputPath = args.first()
         println("Generating TypeScript model interfaces and storing in $outputPath")
 
@@ -32,15 +29,19 @@ fun main(args: Array<String>)
                 rootClasses = setOf(
                         AssociateRole::class,
                         AssociateUser::class,
+                        CohortRestriction::class,
                         CoverageSet::class,
+                        Country::class,
                         Disease::class,
                         DemographicDataset::class,
+                        NumberRange::class,
                         ModellingGroup::class,
                         ModellingGroupDetails::class,
                         ModellingGroupCreation::class,
                         ModelRunParameterSet::class,
                         Report::class,
                         ResponsibilitySet::class,
+                        ResponsibilitySetWithExpectations::class,
                         Result::class,
                         Scenario::class,
                         ScenarioAndCoverageSets::class,
@@ -53,7 +54,8 @@ fun main(args: Array<String>)
                         LocalDateTime::class to "Date",
                         LocalDate::class to "Date",
                         Timestamp::class to "Date",
-                        Instant::class to "string"
+                        Instant::class to "string",
+                        IntRange::class to "NumberRange"
                 ),
                 ignoreSuperclasses = setOf(Iterable::class, HasKey::class),
                 classTransformers = listOf(
@@ -68,10 +70,12 @@ fun main(args: Array<String>)
     }
 }
 
+// A class matching Montagu serialization of Kotlin's IntRange class
+data class NumberRange(val MinimumInclusive: Int, val MaximumInclusive: Int)
+
 fun transformEnum(klass: KClass<*>, value: Any): String {
     var asString = value.toString();
-    asString = when (value)
-    {
+    asString = when (value) {
         GAVISupportLevel.NONE -> "no vaccine"
         GAVISupportLevel.WITHOUT -> "no gavi"
         GAVISupportLevel.WITH -> "total"
@@ -80,10 +84,8 @@ fun transformEnum(klass: KClass<*>, value: Any): String {
     return asString.toLowerCase().replace('_', '-')
 }
 
-object SnakeCaseTransformer : ClassTransformer
-{
-    override fun transformPropertyName(propertyName: String, property: KProperty<*>, klass: KClass<*>): String
-    {
+object SnakeCaseTransformer : ClassTransformer {
+    override fun transformPropertyName(propertyName: String, property: KProperty<*>, klass: KClass<*>): String {
         return camelCaseToSnakeCase(propertyName)
     }
 }
