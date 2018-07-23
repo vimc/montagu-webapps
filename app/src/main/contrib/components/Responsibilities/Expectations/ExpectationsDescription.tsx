28d02a9970f0193df310981a01065f111ba1072d
Alex Hill <alex.hill@gmail.com>
2018-07-23 15:56:16 +0100

    update UI and tests

--- app/src/main/contrib/components/Responsibilities/Expectations/ExpectationsDescription.tsx
+++ app/src/main/contrib/components/Responsibilities/Expectations/ExpectationsDescription.tsx
@@ -2,14 +2,26 @@ import * as React from "react";
 import {ExpectationMapping} from "../../../../shared/models/Generated";
 
 export class ExpectationsDescription extends React.PureComponent<ExpectationMapping> {
+
     render(): JSX.Element {
+
+        const expectation = this.props.expectation;
+        const numCountries = expectation.countries.length;
+        const numYears = expectation.years.maximum_inclusive - expectation.years.minimum_inclusive + 1;
+        const numAges = expectation.ages.maximum_inclusive - expectation.ages.minimum_inclusive + 1;
+        const numCohorts = expectation.cohorts.maximum_birth_year - expectation.cohorts.minimum_birth_year + 1;
+        const numOutcomes = expectation.outcomes.length;
         return <div className="mt-3 mb-5 p-3 border">
             <div className="h3">Template for {this.applicableScenarios()}</div>
             <div>
-                You need to do the following:
+                Expecting data for:
                 <ul>
-                    <li>These years</li>
-                    <li>These ages</li>
+                    <li id={"years"}>{numYears} years: {expectation.years.minimum_inclusive} - {expectation.years.maximum_inclusive}</li>
+                    <li id={"ages"}>{numAges} ages: {expectation.ages.minimum_inclusive} - {expectation.ages.maximum_inclusive}</li>
+                    {expectation.cohorts.minimum_birth_year &&
+                    <li id={"cohorts"}>{numCohorts} cohorts: {expectation.cohorts.minimum_birth_year} - {expectation.cohorts.maximum_birth_year}</li>}
+                    <li id={"countries"}>{numCountries} countries: {expectation.countries.map(c => c.name).join(", ")}</li>
+                    <li id={"outcomes"}>{numOutcomes} outcomes: {expectation.outcomes.join(", ")}</li>
                 </ul>
             </div>
             <div>
--- app/src/test/contrib/components/Responsibilities/Expectations/ExpectationsDescriptionTests.tsx
+++ app/src/test/contrib/components/Responsibilities/Expectations/ExpectationsDescriptionTests.tsx
@@ -1,9 +1,10 @@
-import {ExpectationMapping} from "../../../../../main/shared/models/Generated";
-import {mockExpectations} from "../../../../mocks/mockModels";
-import {ExpectationsDescription} from "../../../../../main/contrib/components/Responsibilities/Expectations/ExpectationsDescription";
 import {shallow} from "enzyme";
 import * as React from "react";
 import {expect} from "chai";
+
+import {ExpectationMapping} from "../../../../../main/shared/models/Generated";
+import {mockCountry, mockExpectations} from "../../../../mocks/mockModels";
+import {ExpectationsDescription} from "../../../../../main/contrib/components/Responsibilities/Expectations/ExpectationsDescription";
 
 describe("ExpectationsDescription", () => {
     it("renders applicable scenarios", () => {
@@ -14,4 +15,59 @@ describe("ExpectationsDescription", () => {
         const rendered = shallow(<ExpectationsDescription {...expectation}/>);
         expect(rendered.find(".h3").text()).to.equal("Template for a, b, c");
     });
+
+    it("renders number of years", () => {
+        const expectation: ExpectationMapping = {
+            expectation: mockExpectations({years: {maximum_inclusive: 2000, minimum_inclusive: 1999}}),
+            applicable_scenarios: ["a", "b", "c"]
+        };
+        const rendered = shallow(<ExpectationsDescription {...expectation}/>);
+        expect(rendered.find("#years").text()).to.equal("2 years: 1999 - 2000");
+    });
+
+    it("renders number of ages", () => {
+        const expectation: ExpectationMapping = {
+            expectation: mockExpectations({ages: {maximum_inclusive: 9, minimum_inclusive: 0}}),
+            applicable_scenarios: ["a", "b", "c"]
+        };
+        const rendered = shallow(<ExpectationsDescription {...expectation}/>);
+        expect(rendered.find("#ages").text()).to.equal("10 ages: 0 - 9");
+    });
+
+    it("renders number of cohorts if present", () => {
+        const expectation: ExpectationMapping = {
+            expectation: mockExpectations({cohorts: {minimum_birth_year: 1980, maximum_birth_year: 1982}}),
+            applicable_scenarios: ["a", "b", "c"]
+        };
+        const rendered = shallow(<ExpectationsDescription {...expectation}/>);
+        expect(rendered.find("#cohorts").text()).to.equal("3 cohorts: 1980 - 1982");
+    });
+
+    it("does not render number of cohorts if null", () => {
+        const expectation: ExpectationMapping = {
+            expectation: mockExpectations({cohorts: {minimum_birth_year: null, maximum_birth_year: null}}),
+            applicable_scenarios: ["a", "b", "c"]
+        };
+        const rendered = shallow(<ExpectationsDescription {...expectation}/>);
+        expect(rendered.find("#cohorts")).to.have.lengthOf(0);
+    });
+
+    it("renders outcomes", () => {
+        const expectation: ExpectationMapping = {
+            expectation: mockExpectations({outcomes: ["deaths", "cases"]}),
+            applicable_scenarios: ["a", "b", "c"]
+        };
+        const rendered = shallow(<ExpectationsDescription {...expectation}/>);
+        expect(rendered.find("#outcomes").text()).to.equal("2 outcomes: deaths, cases")
+    });
+
+    it("renders countries", () => {
+        const expectation: ExpectationMapping = {
+            expectation: mockExpectations({countries: [mockCountry({name: "countrya"}), mockCountry({name: "countryb"})]}),
+            applicable_scenarios: ["a", "b", "c"]
+        };
+        const rendered = shallow(<ExpectationsDescription {...expectation}/>);
+        expect(rendered.find("#countries").text()).to.equal("2 countries: countrya, countryb")
+    });
+
 });
