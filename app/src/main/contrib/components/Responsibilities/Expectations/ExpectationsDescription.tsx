4e16ee505fb490dddb60a1838178fcecbe8488a2
Alex Hill <alex.hill@gmail.com>
2018-07-24 17:31:59 +0100

    countries in popover

--- app/src/main/contrib/components/Responsibilities/Expectations/ExpectationsDescription.tsx
+++ app/src/main/contrib/components/Responsibilities/Expectations/ExpectationsDescription.tsx
@@ -1,7 +1,46 @@
 import * as React from "react";
-import {ExpectationMapping} from "../../../../shared/models/Generated";
+import {Country, ExpectationMapping, NumberRange} from "../../../../shared/models/Generated";
+import {Popover, PopoverBody} from "reactstrap";
 
-export class ExpectationsDescription extends React.PureComponent<ExpectationMapping> {
+interface State {
+    showCountries: boolean;
+}
+
+interface Props {
+    countries: Country[]
+    targetKey: string;
+}
+
+class CountriesList extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            showCountries: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            showCountries: !this.state.showCountries
+        });
+    }
+
+    render() {
+        const countriesList = this.props.countries.map(c => c.name).join(", ");
+
+        return <span><a href="#" id={this.props.targetKey}
+                       onClick={this.toggle}>view list</a>
+            <Popover placement="right" isOpen={this.state.showCountries} target={this.props.targetKey}
+                     toggle={this.toggle}>
+                <PopoverBody>{countriesList}</PopoverBody>
+            </Popover></span>
+    }
+}
+
+export class ExpectationsDescription extends React.Component<ExpectationMapping, State> {
 
     render(): JSX.Element {
 
@@ -11,16 +50,18 @@ export class ExpectationsDescription extends React.PureComponent<ExpectationMapp
         const numAges = expectation.ages.maximum_inclusive - expectation.ages.minimum_inclusive + 1;
         const numCohorts = expectation.cohorts.maximum_birth_year - expectation.cohorts.minimum_birth_year + 1;
         const numOutcomes = expectation.outcomes.length;
+
         return <div className="mt-3 mb-5 p-3 border">
             <div className="h3">Template for {this.applicableScenarios()}</div>
             <div>
                 Expecting data for:
                 <ul>
-                    <li id={"years"}>{numYears} years: {expectation.years.minimum_inclusive} - {expectation.years.maximum_inclusive}</li>
-                    <li id={"ages"}>{numAges} ages: {expectation.ages.minimum_inclusive} - {expectation.ages.maximum_inclusive}</li>
+                    <li id={"years"}>{numYears} years: {rangeAsString(expectation.years)}</li>
+                    <li id={"ages"}>{numAges} ages: {rangeAsString(expectation.ages)}</li>
                     {expectation.cohorts.minimum_birth_year &&
                     <li id={"cohorts"}>{numCohorts} cohorts: {expectation.cohorts.minimum_birth_year} - {expectation.cohorts.maximum_birth_year}</li>}
-                    <li id={"countries"}>{numCountries} countries: {expectation.countries.map(c => c.name).join(", ")}</li>
+                    <li id={"countries"}>{numCountries} countries: <CountriesList targetKey={`countries-${expectation.id}`}
+                                                                                  countries={expectation.countries} /></li>
                     <li id={"outcomes"}>{numOutcomes} outcomes: {expectation.outcomes.join(", ")}</li>
                 </ul>
             </div>
@@ -33,4 +74,8 @@ export class ExpectationsDescription extends React.PureComponent<ExpectationMapp
     applicableScenarios() {
         return this.props.applicable_scenarios.join(", ");
     }
+}
+
+function rangeAsString(range: NumberRange) {
+    return `${range.minimum_inclusive} - ${range.maximum_inclusive}`
 }
