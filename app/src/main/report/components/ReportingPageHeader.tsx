edf3c2aae4931ba2f41177bc8cced0a17cb0f3a1
Martin Eden <martin.s.eden@pm.me>
2018-07-30 13:44:27 +0100

    Fix compile error

--- app/src/main/report/components/ReportingPageHeader.tsx
+++ app/src/main/report/components/ReportingPageHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import {PageHeader} from "../../shared/components/PageWithHeader/PageHeaderNew";
 import * as logo from "./logo-green.png"
+import {PageHeader} from "../../shared/components/PageWithHeader/PageHeader";
 
 export const ReportingPageHeader: React.SFC<undefined> = () => (
     <PageHeader logo={logo} siteTitle={"Reporting portal"} />
