9db0d4920c77a6947b49d5830968566288facd98
Alex Hill <alex.hill@gmail.com>
2018-08-06 13:55:40 +0100

    use switch for old templates

--- app/src/main/shared/settings/default.ts
+++ app/src/main/shared/settings/default.ts
@@ -9,7 +9,8 @@ export const settings: Settings = {
     isApplicantTouchstone: (id: string) => id.indexOf("rfp-") > -1,
     isStochasticTouchstone: (id: string) => id.indexOf("201804rfp") > -1,
     showTouchstoneCreation: false,
-    useNewTemplates: true,
+    showOldTemplates: false,
+    showNewTemplates: false,
     admin: {
         publicPath: "/admin",
         requiresModellingGroupMembership: false
