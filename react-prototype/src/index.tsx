ebdf80dd8288c20549ce984b938da5ae6164c254
Alex Hill <alex.hill@gmail.com>
2018-08-08 12:24:59 +0100

    delete prototype code

--- react-prototype/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import Router from "./Router";
-
-ReactDOM.render(
-    <Router />,
-    document.getElementById("react")
-);
--- react-prototype/src/types/simple-react-router.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "simple-react-router" {
-    import { Component} from 'react';
-
-    export default class SimpleReactRouter extends Component<any, any> {}
-    export class Link extends Component<any, any> {}
-}
