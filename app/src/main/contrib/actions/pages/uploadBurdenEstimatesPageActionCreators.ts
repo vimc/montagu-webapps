4e73d0aa4f9f4d4f77797c5541c3bc3bc7665f06
Alex Hill <alex.hill@gmail.com>
2018-08-09 10:17:29 +0100

    remove unused imports

--- app/src/main/contrib/actions/pages/uploadBurdenEstimatesPageActionCreators.ts
+++ app/src/main/contrib/actions/pages/uploadBurdenEstimatesPageActionCreators.ts
@@ -1,20 +1,14 @@
-import { Dispatch } from "redux";
+import {Dispatch} from "redux";
 
-import {breadcrumbsActionCreators} from "../../../shared/actions/breadcrumbsActionsCreators";
 import {responsibilityOverviewPageActionCreators} from "./responsibilityOverviewPageActionCreators";
-
-import {
-    UploadBurdenEstimatesPageComponent,
-    UploadBurdenEstimatesPageLocationProps,
-} from "../../components/Responsibilities/BurdenEstimates/UploadBurdenEstimatesPage";
+import {UploadBurdenEstimatesPageLocationProps} from "../../components/Responsibilities/BurdenEstimates/UploadBurdenEstimatesPage";
 import {responsibilitiesActionCreators} from "../responsibilitiesActionCreators";
 import {estimatesActionCreators} from "../estimatesActionCreators";
 import {ContribAppState} from "../../reducers/contribAppReducers";
 import {ContribPageActionCreators} from "./ContribPageActionCreators";
 import {PageBreadcrumb} from "../../../shared/components/PageWithHeader/PageProperties";
-import {ResponsibilityOverviewPageComponent} from "../../components/Responsibilities/Overview/ResponsibilityOverviewPage";
 
-class UploadBurdenEstimatesPageActionCreators extends ContribPageActionCreators<UploadBurdenEstimatesPageLocationProps>{
+class UploadBurdenEstimatesPageActionCreators extends ContribPageActionCreators<UploadBurdenEstimatesPageLocationProps> {
     parent = responsibilityOverviewPageActionCreators;
 
     createBreadcrumb(state: ContribAppState): PageBreadcrumb {
@@ -24,7 +18,7 @@ class UploadBurdenEstimatesPageActionCreators extends ContribPageActionCreators<
         }
     }
 
-    loadData(params: UploadBurdenEstimatesPageLocationProps){
+    loadData(params: UploadBurdenEstimatesPageLocationProps) {
         return async (dispatch: Dispatch<ContribAppState>) => {
             dispatch(responsibilitiesActionCreators.setCurrentResponsibility(params.scenarioId));
             await dispatch(estimatesActionCreators.getOneTimeToken());
