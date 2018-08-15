fa35ce1d0a56cbbcc11e1f129574e66db06be691
Alex Hill <alex.hill@gmail.com>
2018-08-15 13:34:48 +0100

    rename param

--- app/src/main/contrib/services/DemographicService.ts
+++ app/src/main/contrib/services/DemographicService.ts
@@ -1,9 +1,9 @@
 import {AbstractLocalService} from "../../shared/services/AbstractLocalService";
 
 export class DemographicService extends AbstractLocalService {
-    getDataSetsByTouchstoneId(touchstoneId: string) {
+    getDataSetsByTouchstoneId(touchstoneVersionId: string) {
         return this.setOptions({cacheKey: DemographicCacheKeysEnum.sets})
-            .get(`/touchstones/${touchstoneId}/demographics/`);
+            .get(`/touchstones/${touchstoneVersionId}/demographics/`);
     }
 }
 
