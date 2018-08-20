cd319c590284f36ca6091accffe26a7f0681c195
Martin Eden <martin.s.eden@pm.me>
2018-08-20 14:00:15 +0100

    Revert test accounts

--- app/scripts/add-test-accounts-for-integration-tests.sh
+++ app/scripts/add-test-accounts-for-integration-tests.sh
@@ -16,12 +16,11 @@ $here/cli.sh addUserToGroup test.user test-group
 $here/cli.sh addRole test.user uploader modelling-group:test-group
 $here/cli.sh addRole test.user user-manager modelling-group:test-group
 
-$here/cli.sh add "Admin user" admin.user \
-    admin@example.com password \
+$here/cli.sh add "Report reviewer" report.reviewer \
+    report.reviewer@example.com password \
     --if-not-exists
 
-$here/cli.sh addRole admin.user user
-$here/cli.sh addRole admin.user reports-reviewer
-$here/cli.sh addRole admin.user user-manager
-$here/cli.sh addRole admin.user reports-reader report:html
-$here/cli.sh addRole admin.user funder
+$here/cli.sh addRole report.reviewer user
+$here/cli.sh addRole report.reviewer reports-reviewer
+$here/cli.sh addRole report.reviewer user-manager
+$here/cli.sh addRole report.reviewer reports-reader report:html
