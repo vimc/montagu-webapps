547e1d6513c8c9e04f4af82b96a49f10e9e9a6ac
Alex Hill <alex.hill@gmail.com>
2018-10-31 17:11:41 +0000

    hide buttons based on admin permissions

--- app/src/main/admin/components/MainMenu/MainMenu.tsx
+++ app/src/main/admin/components/MainMenu/MainMenu.tsx
@@ -5,8 +5,19 @@ import {PageProperties} from "../../../shared/components/PageWithHeader/PageProp
 import {ButtonLink} from "../../../shared/components/ButtonLink";
 
 import "./MainMenu.scss"
+import {AdminPage} from "../../AdminPage";
+import {mainMenuPageActionCreators} from "../../actions/pages/MainMenuPageActionCreators";
+import {compose} from "recompose";
+import {connect} from "react-redux";
+import {AdminAppState} from "../../reducers/adminAppReducers";
 
-export class MainMenu extends React.Component<PageProperties<undefined>> {
+interface Props {
+    canViewUsers: boolean;
+    canViewGroups: boolean;
+    canViewTouchstones: boolean;
+}
+
+export class MainMenuListComponent extends React.Component<Props> {
 
     static title: string = "Main menu";
 
@@ -14,16 +25,41 @@ export class MainMenu extends React.Component<PageProperties<undefined>> {
         return <PageArticle title={MainMenu.title}>
             Please select which area of Montagu you would like to manage:
             <ol className="admin-menu">
-                <li>
+                {this.props.canViewGroups && <li>
                     <ButtonLink href="/modelling-groups/">Modelling groups</ButtonLink>
-                </li>
-                <li>
+                </li>}
+
+                {this.props.canViewUsers && <li>
                     <ButtonLink href="/touchstones/">Touchstones</ButtonLink>
-                </li>
-                <li>
+                </li>}
+
+                {this.props.canViewTouchstones && <li>
                     <ButtonLink href="/users/">Users and permissions</ButtonLink>
-                </li>
+                </li>}
             </ol>
         </PageArticle>;
     }
 }
+
+export const mapStateToProps = (state: AdminAppState): Props => ({
+    canViewGroups: state.auth.permissions.indexOf("modelling-groups.read") > -1,
+    canViewTouchstones: state.auth.permissions.indexOf("touchstones.read") > -1,
+    canViewUsers: state.auth.permissions.indexOf("users.read") > -1
+});
+
+export const MainMenuList = compose(
+    connect(mapStateToProps)
+)(MainMenuListComponent);
+
+
+export class MainMenu extends React.Component<PageProperties<undefined>> {
+
+    static title: string = "Main menu";
+
+    render(): JSX.Element {
+        return <MainMenuList />
+    }
+}
+
+
+export const MainMenuPage = AdminPage(mainMenuPageActionCreators)(MainMenu);
