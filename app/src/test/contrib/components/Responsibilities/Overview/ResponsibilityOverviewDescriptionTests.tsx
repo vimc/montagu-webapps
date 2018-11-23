9604fc233d6eb4b99ffefd4af4b55e82be24304b
Emma Russell <EmmaRussellKilburn@gmail.com>
2018-11-23 14:58:26 +0000

    Fix test

--- app/src/test/contrib/components/Responsibilities/Overview/ResponsibilityOverviewDescriptionTests.tsx
+++ app/src/test/contrib/components/Responsibilities/Overview/ResponsibilityOverviewDescriptionTests.tsx
@@ -12,15 +12,16 @@ describe("Responsibility Overview Description Component", () => {
     const testTouchstoneId1 = "t-1";
     const testTouchstoneId2 = "rfp-1";
     const testGroupId = "g-1";
+    const touchstoneStatus="open";
     afterEach(() => sandbox.restore());
 
     it("renders component on touchstone not applicants", () => {
-        const rendered = shallow(<ResponsibilityOverviewDescription currentTouchstoneId={testTouchstoneId1} groupId={testGroupId}/>);
+        const rendered = shallow(<ResponsibilityOverviewDescription currentTouchstoneId={testTouchstoneId1} groupId={testGroupId} touchstoneStatus={touchstoneStatus}/>);
         expect(rendered.text().indexOf("See an overview of which scenarios") > -1).to.equal(true);
     });
 
     it("renders component on touchstone is applicants", () => {
-        const rendered = shallow(<ResponsibilityOverviewDescription currentTouchstoneId={testTouchstoneId2} groupId={testGroupId}/>);
+        const rendered = shallow(<ResponsibilityOverviewDescription currentTouchstoneId={testTouchstoneId2} groupId={testGroupId} touchstoneStatus={touchstoneStatus}/>);
         expect(rendered.text().indexOf("Access the standardised demographic") > -1).to.equal(true);
     });
 });
