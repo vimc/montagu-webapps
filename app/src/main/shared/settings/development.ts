79eb6eee9d6d48d1ac9ad005baf20fc01799bdc2
Emma Russell <EmmaRussellKilburn@gmail.com>
2018-12-10 16:56:01 +0000

    Removed commented out code

--- app/src/main/shared/settings/development.ts
+++ app/src/main/shared/settings/development.ts
@@ -3,9 +3,4 @@ export const settings: Partial<Settings> = {
     showNewTemplates: true,
     is2017Touchstone: (id: string) => id.indexOf("op-2017-") === 0,
     nonStochasticTouchstones: ["op-2018"]
-    //isVersionOfStochasticTouchstone: function(touchstoneId: string){
-    //    //is touchstone id NOT a version of any name in array of non-stochastic touchstones
-    //    const nonStochasticTouchstones = ["op-2018"];
-    //    return !nonStochasticTouchstones.some((ts: string) => touchstoneId.indexOf(ts) === 0);
-    //},
 };
