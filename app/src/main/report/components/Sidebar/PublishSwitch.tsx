b035b5d09ec773f7bce05e481ccd6aae5948ca96
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-01-30 14:05:09 +0000

    Added confirm modal when un/publishing reports

--- app/src/main/report/components/Sidebar/PublishSwitch.tsx
+++ app/src/main/report/components/Sidebar/PublishSwitch.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Toggle from 'react-bootstrap-toggle';
 import {connect, Dispatch} from "react-redux";
+import * as ReactDOM from "react-dom";
 
 import {ReportAppState} from "../../reducers/reportAppReducers";
 import {UncontrolledTooltip} from "reactstrap";
@@ -12,19 +13,45 @@ export interface PublicProps {
     published: boolean;
 }
 
-export interface Props extends PublicProps {
+export interface PublishSwitchProps extends PublicProps {
     publish: (name: string, version: string) => void;
     unpublish: (name: string, version: string) => void;
 }
 
-export class PublishSwitchComponent extends React.Component<Props, undefined> {
+interface PublishSwitchState {
+    showModal: boolean
+}
+
+export interface PublishSwitchModalProps {
+    show: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+    publishVerb: string;
+}
+
+export class PublishSwitchComponent extends React.Component<PublishSwitchProps, PublishSwitchState> {
 
     constructor() {
         super();
+        this.state = { showModal: false };
         this.onToggle = this.onToggle.bind(this);
+        this.onConfirm = this.onConfirm.bind(this);
     }
 
+    showModal() {
+        this.setState({ showModal: true });
+    }
+
+    hideModal = () => {
+        this.setState({ showModal: false });
+    };
+
     onToggle() {
+        this.showModal();
+    }
+
+    onConfirm() {
+        this.hideModal();
         if (this.props.published) {
             this.props.unpublish(this.props.name, this.props.version)
         }
@@ -33,8 +60,30 @@ export class PublishSwitchComponent extends React.Component<Props, undefined> {
         }
     }
 
+    Modal = (props: PublishSwitchModalProps) => {
+        const displayClassName = props.show ? "modal-background modal-show" : "modal-background modal-hide";
+        return (
+            <div className={displayClassName}>
+                <div className="modal-main px-3 py-3">
+                    <div className={"mb-2 font-weight-bold"}>{`Confirm ${props.publishVerb}`}</div>
+                    <div className={"mb-2"}>{`Are you sure you want to ${props.publishVerb} this report version?`}</div>
+                    <div className={"modal-buttons"}>
+                        <button className={"btn btn-primary mr-3"} onClick={props.onConfirm}>Yes</button>
+                        <button className={"btn btn-default"} onClick={props.onClose}>No</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    getPublishVerb(){
+        return this.props.published ? 'unpublish' : 'publish'
+    }
+
     render() {
         return <div className="pt-3" id={"publish"}>
+                <this.Modal show={this.state.showModal} publishVerb={this.getPublishVerb()} onClose={this.hideModal}
+                    onConfirm={this.onConfirm}/>
                 <Toggle
                     onClick={this.onToggle}
                     on={<span>Published</span>}
@@ -49,7 +98,7 @@ export class PublishSwitchComponent extends React.Component<Props, undefined> {
     }
 }
 
-const mapStateToProps = (state: ReportAppState, props: PublicProps): Partial<Props> => {
+const mapStateToProps = (state: ReportAppState, props: PublicProps): Partial<PublishSwitchProps> => {
     return {
         name: props.name,
         version: props.version,
@@ -57,7 +106,7 @@ const mapStateToProps = (state: ReportAppState, props: PublicProps): Partial<Pro
     };
 };
 
-export const mapDispatchToProps = (dispatch: Dispatch<ReportAppState>): Partial<Props> => {
+export const mapDispatchToProps = (dispatch: Dispatch<ReportAppState>): Partial<PublishSwitchProps> => {
     return {
         publish: (name: string, version: string) => dispatch(reportActionCreators.publishReport(name, version)),
         unpublish: (name: string, version: string) => dispatch(reportActionCreators.unPublishReport(name, version))
