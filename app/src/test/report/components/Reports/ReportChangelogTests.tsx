d78549337cfebf6abd24dcda8afc2ce2b45a8b4d
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-02-01 09:26:12 +0000

    added tests for enabling changelog tab for readers

--- app/src/test/report/components/Reports/ReportChangelogTests.tsx
+++ app/src/test/report/components/Reports/ReportChangelogTests.tsx
@@ -8,7 +8,7 @@ import {
     ReportChangelogComponent,
     ReportChangelogProps
 } from "../../../../main/report/components/Reports/ReportChangelog";
-import {mockReportAppState} from "../../../mocks/mockStates";
+import {mockAuthState, mockReportAppState} from "../../../mocks/mockStates";
 import {Changelog} from "../../../../main/shared/models/Generated"
 
 describe("ReportChangelog", () => {
@@ -33,11 +33,13 @@ describe("ReportChangelog", () => {
                     currentReport: "reportname",
                     versionDetails: {id: "v1"},
                     versions: ["v1", "v2", "v3"],
-                }
+                },
+                auth: mockAuthState({isReportReviewer: true})
             });
 
             const expected: Partial<ReportChangelogProps> = {
-                versionChangelog: expectedChangelog
+                versionChangelog: expectedChangelog,
+                isReviewer: true
             };
             expect(mapStateToProps(reportStateProps)).to.eql(expected);
         });
@@ -48,6 +50,7 @@ describe("ReportChangelog", () => {
         const empty : Changelog[] = [];
         const rendered = shallow(<ReportChangelogComponent
             versionChangelog={empty}
+            isReviewer={true}
             report="reportname"
             version="v1"
             fetchChangelog={null}
@@ -56,7 +59,7 @@ describe("ReportChangelog", () => {
         expect(rendered.find("p").text()).to.eql("There is no Changelog for this Report version.");
     });
 
-    it("renders changelog", () => {
+    it("renders changelog for reviewer", () => {
         const expectedChangelog =[
             {
                 report_version: "20180123-155855-5d5b8238",
@@ -76,8 +79,11 @@ describe("ReportChangelog", () => {
             report="reportname"
             version="v2"
             fetchChangelog={null}
+            isReviewer={true}
         />);
         expect(rendered.find("h3").text()).to.eql("Changelog");
+
+        //Do expect to find label column
 
         const tableHeaders = rendered.find("th");
         expect(tableHeaders.at(0).text()).to.eql("Date");
@@ -97,5 +103,46 @@ describe("ReportChangelog", () => {
         expect(secondRowCells.at(1).text()).to.eql("internal");
         expect(secondRowCells.at(1).find("span").hasClass("badge-internal")).to.eql(true);
         expect(secondRowCells.at(2).text()).to.eql("internal test changelog message");
+    });
+
+    it("renders changelog for reader", () => {
+        const expectedChangelog =[
+            {
+                report_version: "20180123-155855-5d5b8238",
+                label:"public",
+                value:"public test changelog message",
+                from_file: true
+            },
+            {
+                report_version: "20180104-082959-0544c986",
+                label:"internal",
+                value:"internal test changelog message",
+                from_file: false
+            }
+        ];
+        const rendered = shallow(<ReportChangelogComponent
+            versionChangelog={expectedChangelog}
+            report="reportname"
+            version="v2"
+            fetchChangelog={null}
+            isReviewer={false}
+        />);
+        expect(rendered.find("h3").text()).to.eql("Changelog");
+
+        //Don't expect to find label column
+
+        const tableHeaders = rendered.find("th");
+        expect(tableHeaders.at(0).text()).to.eql("Date");
+        expect(tableHeaders.at(1).text()).to.eql("Text");
+
+        const firstRow = rendered.find("tbody tr").first();
+        const firstRowCells = firstRow.find("td");
+        expect(firstRowCells.at(0).text()).to.eql("Tue Jan 23 2018, 15:58");
+        expect(firstRowCells.at(1).text()).to.eql("public test changelog message");
+
+        const secondRow = rendered.find("tbody tr").at(1);
+        const secondRowCells = secondRow.find("td");
+        expect(secondRowCells.at(0).text()).to.eql("Thu Jan 04 2018, 08:29");
+        expect(secondRowCells.at(1).text()).to.eql("internal test changelog message");
     });
 });
