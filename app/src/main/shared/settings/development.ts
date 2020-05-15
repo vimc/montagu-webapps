932245f5f53c6f7793eda754634daf5973601aa1
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-05-15 14:18:58 +0100

    Extraneous comma

--- app/src/main/shared/settings/development.ts
+++ app/src/main/shared/settings/development.ts
@@ -1,5 +1,5 @@
 export const settings: Partial<Settings> = {
     showTouchstoneCreation: true,
     is2017Touchstone: (id: string) => id.indexOf("op-2017-") === 0,
-    nonStochasticTouchstones: ["op-2018"],
+    nonStochasticTouchstones: ["op-2018"]
 };
