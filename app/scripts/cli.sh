8bec568a42ac0c470f24121b9344027fbefa593a
Alex <alex.hill@gmail.com>
2020-07-28 09:59:13 +0100

    use master branch for the cli which doesn't change

--- app/scripts/cli.sh
+++ app/scripts/cli.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 here=$(dirname $0)
-api_version=$(<$here/../../config/api_version)
 
 # Use this once you have run the API with ./run-dependencies.sh
-image=docker.montagu.dide.ic.ac.uk:5000/montagu-cli:$api_version
+image=docker.montagu.dide.ic.ac.uk:5000/montagu-cli:master
 exec docker run --network montagu_default $image "$@"
