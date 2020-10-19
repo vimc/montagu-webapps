13cb25996f7b4957f2e9884135a9b22ea18f7bf1
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-10-19 16:12:35 +0100

    Pull the node docker image

--- buildkite/make-build-env.sh
+++ buildkite/make-build-env.sh
@@ -3,6 +3,8 @@ set -e
 HERE=$(dirname $0)
 . $HERE/common
 
+docker pull vimc/node-docker-openjdk:master
+
 # Shared build env between the main build (being executed now) and the
 # reusable integration test image that will be run in the main Montagu build.
 docker build -f ./docker/shared-build-env.dockerfile \
