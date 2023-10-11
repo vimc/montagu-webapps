6710389827c0d7fc18b141e95a51a65f48204cee
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-10-11 16:21:54 +0100

    add component test

--- app/src/main/shared/settings/development.ts
+++ app/src/main/shared/settings/development.ts
@@ -1,6 +1,5 @@
 export const settings: Partial<Settings> = {
     showTouchstoneCreation: true,
     is2017Touchstone: (id: string) => id.indexOf("op-2017-") === 0,
-    isLatestGuidanceTouchstone: (id: string) => id.indexOf("op-2018-") === 0,
-    nonStochasticTouchstones: ["op-2018"]
+    isLatestGuidanceTouchstone: (id: string) => id.indexOf("op-2018-") === 0
 };
